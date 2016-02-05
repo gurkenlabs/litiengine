827cda815bbb276768bde5d7498e2b778c0afa90
gurkenlabs <gurkenlabs@wibemedia.de>
2016-02-05 20:10:24 +0000

    Updates are now only sent when the entity is within a certain radius. When the entity was previously not within the vision, the client explicitly requests an update of the entity.
    git-svn-id: https://dev.wibe.media/svn/code/java/trunk/litiengine@1328 16493bb7-3577-b145-b9c3-72a97d279fb6

--- src/de/gurkenlabs/litiengine/entities/Direction.java
+++ src/de/gurkenlabs/litiengine/entities/Direction.java
@@ -1,9 +1,29 @@
 package de.gurkenlabs.litiengine.entities;
 
 public enum Direction {
-  DOWN,
-  LEFT,
-  RIGHT,
-  UNDEFINED,
-  UP
+  DOWN((byte) 1),
+  LEFT((byte) 2),
+  RIGHT((byte) 4),
+  UNDEFINED((byte) 8),
+  UP((byte) 16);
+
+  private final byte flagValue;
+
+  private Direction(final byte flagValue) {
+    this.flagValue = flagValue;
+  }
+
+  public byte getFlagValue() {
+    return this.flagValue;
+  }
+
+  public static Direction fromFlagValue(byte flagValue) {
+    for (Direction dir : Direction.values()) {
+      if (dir.getFlagValue() == flagValue) {
+        return dir;
+      }
+    }
+
+    return UNDEFINED;
+  }
 }
