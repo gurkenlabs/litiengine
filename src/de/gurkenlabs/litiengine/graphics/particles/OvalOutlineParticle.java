2de1325d09408a1842468ac19e53e1b9b0c580a2
gurkenlabs <gurkenlabs@wibemedia.de>
2016-04-16 04:29:04 +0000

    git-svn-id: https://dev.wibe.media/svn/code/java/trunk/litiengine@1644 16493bb7-3577-b145-b9c3-72a97d279fb6

---
+++ src/de/gurkenlabs/litiengine/graphics/particles/OvalOutlineParticle.java
@@ -0,0 +1,19 @@
+package de.gurkenlabs.litiengine.graphics.particles;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
+
+public class OvalOutlineParticle extends Particle {
+  public OvalOutlineParticle(final float xCurrent, final float yCurrent, final float dx, final float dy, final float gravityX, final float gravityY, final float width, final float height, final int life, final Color color) {
+    super(xCurrent, yCurrent, dx, dy, gravityX, gravityY, width, height, life, color);
+  }
+
+  @Override
+  public void render(final Graphics2D g, final Point2D emitterOrigin) {
+    final Point2D renderLocation = this.getLocation(emitterOrigin);
+    g.setColor(this.getColor());
+    g.draw(new Ellipse2D.Double(renderLocation.getX(), renderLocation.getY(), this.getWidth(), this.getHeight()));
+  }
+}
