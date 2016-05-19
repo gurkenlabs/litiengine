a9e991c36a938f5bfebfc4dc2155e4485c65842c
gurkenlabs <gurkenlabs@wibemedia.de>
2016-05-19 13:27:38 +0000

    git-svn-id: https://dev.wibe.media/svn/code/java/trunk/litiengine@1855 16493bb7-3577-b145-b9c3-72a97d279fb6

--- src/de/gurkenlabs/litiengine/GameMetrics.java
+++ src/de/gurkenlabs/litiengine/GameMetrics.java
@@ -188,6 +188,12 @@ public class GameMetrics implements IUpdateable, IRenderable {
 
     g.setColor(Color.RED);
     g.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 10));
+    Runtime runtime = Runtime.getRuntime();
+    final float usedMemory = Float.valueOf(Math.round((runtime.totalMemory() - runtime.freeMemory()) / (1024f * 1024f) * 10) * 0.1f);
+    final String memory = "memory: " + usedMemory + "MB";
+    g.drawString(memory, OffsetX, currentOffsetY);
+    currentOffsetY += OffsetY;
+
     final String ping = "ping: " + this.getPing() + "ms";
     g.drawString(ping, OffsetX, currentOffsetY);
     currentOffsetY += OffsetY;
