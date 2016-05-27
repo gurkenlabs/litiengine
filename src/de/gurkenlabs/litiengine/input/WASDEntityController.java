8b2fffeb9b4df3ab9d892ac7cf50e26d02cb0686
gurkenlabs <gurkenlabs@wibemedia.de>
2016-05-27 08:38:35 +0000

    Simplified the WASDEntityController.
    git-svn-id: https://dev.wibe.media/svn/code/java/trunk/litiengine@1869 16493bb7-3577-b145-b9c3-72a97d279fb6

--- src/de/gurkenlabs/litiengine/input/WASDEntityController.java
+++ src/de/gurkenlabs/litiengine/input/WASDEntityController.java
@@ -1,18 +1,18 @@
 package de.gurkenlabs.litiengine.input;
 
 import java.awt.event.KeyEvent;
+import java.awt.geom.Point2D;
 
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.IGameLoop;
-import de.gurkenlabs.litiengine.entities.Direction;
 import de.gurkenlabs.litiengine.entities.IMovableEntity;
 
 public class WASDEntityController extends ClientEntityMovementController implements IKeyObserver {
   private float stepSize;
 
-  // UP, LEFT, DOWN, RIGHT
-  private final boolean[] pressed = new boolean[] { false, false, false, false };
   private boolean moved;
+  private float dx;
+  private float dy;
 
   public WASDEntityController(final IMovableEntity entity, final float stepSize) {
     super(entity);
@@ -25,72 +25,11 @@ public class WASDEntityController extends ClientEntityMovementController impleme
     super.update(loop);
 
     if (this.moved) {
-      // UP-DOWN
-      if (this.pressed[0] && this.pressed[2]) {
-        this.pressed[0] = false;
-        this.pressed[2] = false;
-      }
-
-      // LEFT-RIGHT
-      if (this.pressed[1] && this.pressed[3]) {
-        this.pressed[1] = false;
-        this.pressed[3] = false;
-      }
-
-      // UP-LEFT
-      if (this.pressed[0] && this.pressed[1]) {
-        Game.getPhysicsEngine().move(this.getControlledEntity(), (Direction.toAngle(Direction.UP) + Direction.toAngle(Direction.LEFT)) / 2.0f, this.stepSize);
-        this.pressed[0] = false;
-        this.pressed[1] = false;
-      }
-
-      // UP-RIGHT
-      if (this.pressed[0] && this.pressed[3]) {
-        Game.getPhysicsEngine().move(this.getControlledEntity(), (Direction.toAngle(Direction.UP) + Direction.toAngle(Direction.RIGHT)) / 2.0f, this.stepSize);
-        this.pressed[0] = false;
-        this.pressed[3] = false;
-      }
-
-      // DOWN-LEFT
-      if (this.pressed[2] && this.pressed[1]) {
-        Game.getPhysicsEngine().move(this.getControlledEntity(), (Direction.toAngle(Direction.DOWN) + Direction.toAngle(Direction.LEFT)) / 2.0f, this.stepSize);
-        this.pressed[2] = false;
-        this.pressed[1] = false;
-      }
-
-      // DOWN-RIGHT
-      if (this.pressed[2] && this.pressed[3]) {
-        Game.getPhysicsEngine().move(this.getControlledEntity(), (Direction.toAngle(Direction.DOWN) + Direction.toAngle(Direction.RIGHT)) % 360 / 2.0f, this.stepSize);
-        this.pressed[2] = false;
-        this.pressed[3] = false;
-      }
-
-      for (int i = 0; i < this.pressed.length; i++) {
-        if (!this.pressed[i]) {
-          continue;
-        }
-
-        this.pressed[i] = false;
-        
-        Direction dir = Direction.UNDEFINED;
-        switch (i) {
-        case 0:
-          dir = Direction.UP;
-          break;
-        case 1:
-          dir = Direction.LEFT;
-          break;
-        case 2:
-          dir = Direction.DOWN;
-          break;
-        case 3:
-          dir = Direction.RIGHT;
-          break;
-        }
-        
-        Game.getPhysicsEngine().move(this.getControlledEntity(), Direction.toAngle(dir), this.stepSize);
-      }
+      Point2D newLocation = new Point2D.Double(this.getControlledEntity().getLocation().getX() + this.dx, this.getControlledEntity().getLocation().getY() + this.dy);
+      Game.getPhysicsEngine().move(this.getControlledEntity(), newLocation, this.stepSize);
       
+      this.dx = 0;
+      this.dy = 0;
       this.moved = false;
     }
   }
@@ -100,16 +39,16 @@ public class WASDEntityController extends ClientEntityMovementController impleme
 
     switch (keyCode) {
     case KeyEvent.VK_W:
-      this.pressed[0] = true;
+      this.dy -= this.stepSize;
       break;
     case KeyEvent.VK_A:
-      this.pressed[1] = true;
+      this.dx -= this.stepSize;
       break;
     case KeyEvent.VK_S:
-      this.pressed[2] = true;
+      this.dy += this.stepSize;
       break;
     case KeyEvent.VK_D:
-      this.pressed[3] = true;
+      this.dx += this.stepSize;
       break;
     }
 
