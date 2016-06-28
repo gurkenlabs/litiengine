c8b2be5f99799cc17ff96e843b30b836709af653
gurkenlabs <gurkenlabs@wibemedia.de>
2016-06-28 22:04:52 +0000

    Improved resource appereance.
    git-svn-id: https://dev.wibe.media/svn/code/java/trunk/litiengine@1909 16493bb7-3577-b145-b9c3-72a97d279fb6

--- src/de/gurkenlabs/litiengine/graphics/particles/RectangleOutlineParticle.java
+++ src/de/gurkenlabs/litiengine/graphics/particles/RectangleOutlineParticle.java
@@ -1,12 +1,14 @@
 package de.gurkenlabs.litiengine.graphics.particles;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Stroke;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 public class RectangleOutlineParticle extends Particle {
-
+  private float stroke = 0.5f;
   public RectangleOutlineParticle(final float xCurrent, final float yCurrent, final float dx, final float dy, final float gravityX, final float gravityY, final float width, final float height, final int life, final Color color) {
     super(xCurrent, yCurrent, dx, dy, gravityX, gravityY, width, height, life, color);
   }
@@ -14,7 +16,18 @@ public class RectangleOutlineParticle extends Particle {
   @Override
   public void render(final Graphics2D g, final Point2D emitterOrigin) {
     final Point2D renderLocation = this.getLocation(emitterOrigin);
+    Stroke oldStroke = g.getStroke();
     g.setColor(this.getColor());
+    g.setStroke(new BasicStroke(this.stroke));
     g.draw(new Rectangle2D.Double(renderLocation.getX(), renderLocation.getY(), this.getWidth(), this.getHeight()));
+    g.setStroke(oldStroke);
+  }
+
+  public float getStroke() {
+    return this.stroke;
+  }
+
+  public void setStroke(float stroke) {
+    this.stroke = stroke;
   }
 }
