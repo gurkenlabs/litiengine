e5d611dd4a4dbfc924eec98912a61d730639523c
Gurkenlabs <gurkenlabs@wibemedia.de>
2017-09-09 14:05:41 +0200

    Remove old pathfinding test.

--- tests/de/gurkenlabs/litiengine/physics/pathfinding/PathFindingTests.java
+++ tests/de/gurkenlabs/litiengine/physics/pathfinding/PathFindingTests.java
@@ -15,34 +15,6 @@ import de.gurkenlabs.util.geom.PointDistanceComparator;
 public class PathFindingTests {
 
   @Test
-  public void testGetIntersectionPoints() {
-
-    final Rectangle2D rect = new Rectangle2D.Double(1, 1, 3, 2);
-
-    final Line2D line = new Line2D.Double(0, 0, 4, 4);
-    final ArrayList<Point2D> points = GeometricUtilities.getIntersectionPoints(line, rect);
-    final Point2D expected1 = new Point2D.Double(1, 1);
-    final Point2D expected2 = new Point2D.Double(3, 3);
-    Assert.assertArrayEquals(new Point2D[] { expected1, expected2 }, points.toArray());
-
-    final Line2D line2 = new Line2D.Double(1, 1, 1, 4);
-    final ArrayList<Point2D> points2 = GeometricUtilities.getIntersectionPoints(line2, rect);
-    final Point2D expected3 = new Point2D.Double(1, 1);
-    final Point2D expected4 = new Point2D.Double(1, 3);
-    Assert.assertArrayEquals(new Point2D[] { expected3, expected4 }, points2.toArray());
-
-    final Line2D line3 = new Line2D.Double(0, 0, 1, 1);
-    final ArrayList<Point2D> points3 = GeometricUtilities.getIntersectionPoints(line3, rect);
-    final Point2D expected5 = new Point2D.Double(1, 1);
-    Assert.assertArrayEquals(new Point2D[] { expected5 }, points3.toArray());
-
-    final Line2D line4 = new Line2D.Double(0, 0, 2, 2);
-    final ArrayList<Point2D> points4 = GeometricUtilities.getIntersectionPoints(line4, rect);
-    final Point2D expected6 = new Point2D.Double(1, 1);
-    Assert.assertArrayEquals(new Point2D[] { expected6 }, points4.toArray());
-  }
-
-  @Test
   public void testPointDistanceComparator() {
 
     final Point2D[] points = new Point2D[4];
