527a7c60eb1d9f04839f103dcc31565f1e3cda4d
Gurkenlabs <gurkenlabs@wibemedia.de>
2017-09-12 16:36:39 +0200

    Extend the string resource to support language files that are encoded with UTF-8.

--- src/de/gurkenlabs/litiengine/Resources.java
+++ src/de/gurkenlabs/litiengine/Resources.java
@@ -2,6 +2,7 @@ package de.gurkenlabs.litiengine;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
@@ -10,12 +11,24 @@ import java.util.logging.Logger;
 public final class Resources {
   public static final String LOCALIZATION_RESOURCE_FOLDER = "localization/";
   public static final String DEFAULT_BUNDLE = "strings";
+  public static final String ENCODING_ISO_8859_1 = "ISO-8859-1";
+  public static final String ENCODING_UTF_8 = "UTF-8";
+
+  private static String encoding = ENCODING_ISO_8859_1;
   private static final Logger log = Logger.getLogger(Resources.class.getName());
   static {
     Locale.setDefault(new Locale("en", "US"));
   }
 
   private Resources() {
+  }
+
+  public static void setEncoding(String newEncoding) {
+    if (newEncoding == null || newEncoding.isEmpty()) {
+      throw new IllegalArgumentException("The encoding must not be null or empty.");
+    }
+
+    encoding = newEncoding;
   }
 
   public static String get(final String key) {
@@ -31,8 +44,11 @@ public final class Resources {
     }
     try {
       final ResourceBundle defaultBundle = ResourceBundle.getBundle(LOCALIZATION_RESOURCE_FOLDER + bundleName, Game.getConfiguration().client().getLocale());
-      return defaultBundle.getString(key);
-    } catch (final MissingResourceException me) {
+
+      String value = defaultBundle.getString(key);
+
+      return encoding.equals(ENCODING_ISO_8859_1) ? value : new String(value.getBytes(ENCODING_ISO_8859_1), encoding);
+    } catch (final MissingResourceException | UnsupportedEncodingException me) {
       final StringWriter sw = new StringWriter();
       me.printStackTrace(new PrintWriter(sw));
       final String stacktrace = sw.toString();
