acdd1c290f5c2dee627f5dc9f9f2a415f0a4a6d4
Gurkenlabs <gurkenlabs@wibemedia.de>
2017-10-16 22:41:20 +0200

    Add additional test case for setting custom properties by a collection.

--- tests/de/gurkenlabs/litiengine/environment/tilemap/xml/CustomPropertyProviderTests.java
+++ tests/de/gurkenlabs/litiengine/environment/tilemap/xml/CustomPropertyProviderTests.java
@@ -1,5 +1,7 @@
 package de.gurkenlabs.litiengine.environment.tilemap.xml;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import junit.framework.Assert;
@@ -18,5 +20,15 @@ public class CustomPropertyProviderTests {
     propProvider.setCustomProperty("test", "testvalue2");
 
     Assert.assertEquals("testvalue2", propProvider.getCustomProperty("test"));
+
+    ArrayList<Property> props = new ArrayList<>();
+    props.add(new Property("test2", "testvalue3"));
+    props.add(new Property("test3", "testvalue4"));
+
+    propProvider.setCustomProperties(props);
+
+    Assert.assertEquals(2, propProvider.getAllCustomProperties().size());
+    Assert.assertEquals("testvalue3", propProvider.getCustomProperty("test2"));
+    Assert.assertEquals("testvalue4", propProvider.getCustomProperty("test3"));
   }
 }
