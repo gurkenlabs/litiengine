748e92535c66af94ed9ce0ee6409134ce8cb3df5
Gurkenlabs <gurkenlabs@wibemedia.de>
2017-10-20 19:36:37 +0200

    Extend the MovableCombatEntityAnimationController to allow flipping left/right sprites to provide the opposite. Implement update method that sets the acutal animation.

--- src/de/gurkenlabs/litiengine/graphics/animation/MovableCombatEntityAnimationController.java
+++ src/de/gurkenlabs/litiengine/graphics/animation/MovableCombatEntityAnimationController.java
@@ -1,64 +1,153 @@
 package de.gurkenlabs.litiengine.graphics.animation;
 
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.IGameLoop;
 import de.gurkenlabs.litiengine.entities.Direction;
 import de.gurkenlabs.litiengine.entities.IMovableCombatEntity;
 import de.gurkenlabs.litiengine.graphics.Spritesheet;
+import de.gurkenlabs.util.ImageProcessing;
 
 /**
- * This {@link AnimationController} implementation provides animation rules that use naming conventions to provide {@link Animation}s for
+ * This {@link AnimationController} implementation provides animation rules that
+ * use naming conventions to provide {@link Animation}s for
  * {@link IMovableCombatEntity} implementations.
  * 
- * The spritesheet images need to be named according to the following conventions in order to be automatically used by this controller:
+ * The spritesheet images need to be named according to the following
+ * conventions in order to be automatically used by this controller:
  * <ul>
  * <li>{entity-name}-idle-{DIRECTION}.png</li>
  * <li>{entity-name}-walk-{DIRECTION}.png</li>
  * </ul>
- * Where {DIRECTION} refers to a value of the {@link Direction} enum and {entity-name} refers to the
- * name of the entity.
+ * Where {DIRECTION} refers to a value of the {@link Direction} enum and
+ * {entity-name} refers to the name of the entity.
  * 
  * @see de.gurkenlabs.litiengine.entities.IMovableCombatEntity
  * @see de.gurkenlabs.litiengine.entities.Direction
  * @see de.gurkenlabs.litiengine.entities.IEntity#getName()
  */
 public class MovableCombatEntityAnimationController extends EntityAnimationController<IMovableCombatEntity> {
-  public static final String WALK_IDENTIFIER = "walk";
-  public static final String IDLE_IDENTIFIER = "idle";
 
   public MovableCombatEntityAnimationController(IMovableCombatEntity entity, Animation defaultAnimation) {
+    this(entity, defaultAnimation, true);
+  }
+
+  public MovableCombatEntityAnimationController(IMovableCombatEntity entity, Animation defaultAnimation, boolean useFlippedSpritesAsFallback) {
     super(entity, defaultAnimation);
     if (this.getEntity().getName() == null || this.getEntity().getName().isEmpty()) {
       throw new IllegalArgumentException("Make sure the name of the entity is set before using a MovableCombatEntityAnimationController.");
     }
 
-    this.getAnimations().addAll(this.initializeAvailableAnimations(this.getEntity()));
+    this.getAnimations().addAll(this.initializeAvailableAnimations());
+
+    if (useFlippedSpritesAsFallback) {
+      this.getAnimations().addAll(this.initializeFlippedAnimations());
+    }
   }
 
-  private List<Animation> initializeAvailableAnimations(IMovableCombatEntity entity) {
+  @Override
+  public void update(final IGameLoop gameLoop) {
+    super.update(gameLoop);
 
-    List<Animation> animation = new ArrayList<>();
+    if (Game.getEnvironment() == null || Game.getEnvironment().getMap() == null) {
+      return;
+    }
+    if (this.getCurrentAnimation() != null && !this.getCurrentAnimation().isLoop()) {
+      return;
+    }
+    if (this.getEntity() == null) {
+      return;
+    }
+
+    String animationName = this.getEntity().isIdle() ? this.getIdleSpriteName(this.getEntity().getFacingDirection()) : this.getWalkSpriteName(this.getEntity().getFacingDirection());
+    if (this.getCurrentAnimation() == null || animationName != null && !animationName.isEmpty() && !this.getCurrentAnimation().getName().equalsIgnoreCase(animationName)) {
+      this.playAnimation(animationName);
+    }
+  }
+
+  private List<Animation> initializeAvailableAnimations() {
+    List<Animation> animations = new ArrayList<>();
 
     for (Direction dir : Direction.values()) {
 
       // initialize walking animations
-      final String walkAnimationName = entity.getName().toLowerCase() + "-" + WALK_IDENTIFIER + "-" + dir.toString().toLowerCase();
-      Spritesheet walkSprite = Spritesheet.find(walkAnimationName);
+      Spritesheet walkSprite = getWalkSprite(dir);
       if (walkSprite != null) {
-        animation.add(new Animation(walkSprite, true));
-        this.addAnimationRule(e -> !e.isIdle() && e.getFacingDirection() == dir, walkAnimationName);
+        animations.add(new Animation(walkSprite, true));
+        this.addAnimationRule(e -> !e.isIdle() && e.getFacingDirection() == dir, walkSprite.getName());
       }
 
       // initialize idle animations
-      final String idleAnimationName = entity.getName().toLowerCase() + "-" + IDLE_IDENTIFIER + "-" + dir.toString().toLowerCase();
-      Spritesheet idleSprite = Spritesheet.find(idleAnimationName);
+      Spritesheet idleSprite = getIdleSprite(dir);
       if (idleSprite != null) {
-        animation.add(new Animation(idleSprite, true));
-        this.addAnimationRule(e -> e.isIdle() && e.getFacingDirection() == dir, idleAnimationName);
+        animations.add(new Animation(idleSprite, true));
+        this.addAnimationRule(e -> e.isIdle() && e.getFacingDirection() == dir, idleSprite.getName());
       }
     }
 
-    return animation;
+    return animations;
+  }
+
+  private List<Animation> initializeFlippedAnimations() {
+    List<Animation> animations = new ArrayList<>();
+    String leftIdle = getIdleSpriteName(Direction.LEFT);
+    String leftWalk = getWalkSpriteName(Direction.LEFT);
+    Optional<Animation> leftIdleAnimation = this.getAnimations().stream().filter(x -> x.getName().equals(leftIdle)).findFirst();
+    Optional<Animation> leftWalkAnimation = this.getAnimations().stream().filter(x -> x.getName().equals(leftWalk)).findFirst();
+
+    String rightIdle = getIdleSpriteName(Direction.RIGHT);
+    String rightWalk = getWalkSpriteName(Direction.RIGHT);
+    Optional<Animation> rightIdleAnimation = this.getAnimations().stream().filter(x -> x.getName().equals(rightIdle)).findFirst();
+    Optional<Animation> rightWalkAnimation = this.getAnimations().stream().filter(x -> x.getName().equals(rightWalk)).findFirst();
+
+    if (!leftIdleAnimation.isPresent() && rightIdleAnimation.isPresent()) {
+      animations.add(flipAnimation(rightIdleAnimation.get().getSpritesheet(), leftIdle));
+    }
+
+    if (!leftWalkAnimation.isPresent() && rightWalkAnimation.isPresent()) {
+      animations.add(flipAnimation(rightWalkAnimation.get().getSpritesheet(), leftWalk));
+    }
+
+    if (!rightIdleAnimation.isPresent() && leftIdleAnimation.isPresent()) {
+      animations.add(flipAnimation(leftIdleAnimation.get().getSpritesheet(), rightIdle));
+    }
+
+    if (!rightWalkAnimation.isPresent() && leftWalkAnimation.isPresent()) {
+      animations.add(flipAnimation(leftWalkAnimation.get().getSpritesheet(), rightWalk));
+    }
+
+    return animations;
+  }
+
+  private Animation flipAnimation(Spritesheet spriteToFlip, String newSpriteName) {
+    final BufferedImage leftIdleSprite = ImageProcessing.flipSpritesHorizontally(spriteToFlip);
+    Spritesheet leftIdleSpritesheet = Spritesheet.load(leftIdleSprite, newSpriteName, spriteToFlip.getSpriteWidth(), spriteToFlip.getSpriteHeight());
+    return new Animation(leftIdleSpritesheet, true);
+  }
+
+  private Spritesheet getIdleSprite(Direction dir) {
+    return Spritesheet.find(getIdleSpriteName(dir));
+  }
+
+  private Spritesheet getWalkSprite(Direction dir) {
+    return Spritesheet.find(getWalkSpriteName(dir));
+  }
+
+  private String getIdleSpriteName(Direction dir) {
+    final String spriteIdle = "%s-idle-%s";
+    return getSpriteName(spriteIdle, dir);
+  }
+
+  private String getWalkSpriteName(Direction dir) {
+    final String spriteWalk = "%s-walk-%s";
+    return getSpriteName(spriteWalk, dir);
+  }
+
+  private String getSpriteName(String formatString, Direction dir) {
+    return String.format(formatString, this.getEntity().getName().toLowerCase(), dir.toString().toLowerCase());
   }
 }
