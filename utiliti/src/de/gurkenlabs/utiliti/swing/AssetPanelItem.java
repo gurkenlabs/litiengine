44ac3ed4ee91daaa0fc2a181e34b37a12931ff21
Gurkenlabs <gurkenlabs@wibemedia.de>
2018-01-23 18:16:26 +0100

    Implement add and delete buttons for AssetPanelItems.

--- utiliti/src/de/gurkenlabs/utiliti/swing/AssetPanelItem.java
+++ utiliti/src/de/gurkenlabs/utiliti/swing/AssetPanelItem.java
@@ -37,6 +37,7 @@ import de.gurkenlabs.litiengine.graphics.Spritesheet;
 import de.gurkenlabs.utiliti.EditorScreen;
 import de.gurkenlabs.utiliti.Program;
 import de.gurkenlabs.utiliti.swing.dialogs.SpritesheetImportPanel;
+import java.awt.GridLayout;
 
 public class AssetPanelItem extends JPanel {
   private static final Border normalBorder = BorderFactory.createEmptyBorder(1, 1, 1, 1);
@@ -45,6 +46,8 @@ public class AssetPanelItem extends JPanel {
   private final JLabel iconLabel;
   private final JTextField textField;
   private final JButton btnEdit;
+  private final JButton btnDelete;
+  private final JButton btnAdd;
 
   private final Object origin;
 
@@ -53,6 +56,7 @@ public class AssetPanelItem extends JPanel {
   }
 
   public AssetPanelItem(Object origin) {
+    setPreferredSize(new Dimension(64, 100));
     this.origin = origin;
     this.setBackground(Color.DARK_GRAY);
     this.setBorder(normalBorder);
@@ -60,19 +64,8 @@ public class AssetPanelItem extends JPanel {
     this.getInputMap(JPanel.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0), "deleteAsset");
     this.getActionMap().put("deleteAsset", new AbstractAction() {
       public void actionPerformed(ActionEvent ae) {
-        if (getOrigin() instanceof SpriteSheetInfo) {
-          SpriteSheetInfo info = (SpriteSheetInfo) getOrigin();
-          int n = JOptionPane.showConfirmDialog(null, "Do you really want to delete the spritesheet [" + info.getName() + "]?\n Entities that use the sprite won't be rendered anymore!", "Delete Spritesheet?", JOptionPane.YES_NO_OPTION);
+        deleteAsset();
 
-          if (n == JOptionPane.OK_OPTION) {
-            EditorScreen.instance().getGameFile().getSpriteSheets().remove(getOrigin());
-            ImageCache.clearAll();
-            Spritesheet.remove(info.getName());
-            EditorScreen.instance().getMapComponent().reloadEnvironment();
-
-            Program.getAssetTree().forceUpdate();
-          }
-        }
       }
     });
 
@@ -86,6 +79,8 @@ public class AssetPanelItem extends JPanel {
         setBorder(focusBorder);
         if (getOrigin() instanceof SpriteSheetInfo) {
           btnEdit.setVisible(true);
+          btnAdd.setVisible(true);
+          btnDelete.setVisible(true);
         }
       }
 
@@ -98,6 +93,8 @@ public class AssetPanelItem extends JPanel {
         setBorder(normalBorder);
         if (getOrigin() instanceof SpriteSheetInfo) {
           btnEdit.setVisible(false);
+          btnAdd.setVisible(false);
+          btnDelete.setVisible(false);
         }
       }
     });
@@ -113,6 +110,7 @@ public class AssetPanelItem extends JPanel {
     });
 
     this.iconLabel = new JLabel("");
+    iconLabel.setPreferredSize(new Dimension(64, 64));
     this.iconLabel.setSize(64, 64);
     this.iconLabel.setMinimumSize(new Dimension(64, 64));
     this.iconLabel.addMouseListener(new MouseAdapter() {
@@ -126,34 +124,8 @@ public class AssetPanelItem extends JPanel {
     this.iconLabel.addMouseListener(new MouseAdapter() {
       @Override
       public void mouseClicked(MouseEvent e) {
-        if (e.getClickCount() == 2) {
-
-          // TODO: experimental code... this needs to be refactored with issue #66
-          if (getOrigin() instanceof SpriteSheetInfo) {
-            SpriteSheetInfo info = (SpriteSheetInfo) getOrigin();
-            String propName = Prop.getNameBySpriteName(info.getName());
-            if (propName == null) {
-              return;
-            }
-
-            MapObject mo = new MapObject();
-            mo.setType(MapObjectType.PROP.name());
-            mo.setX((int) Game.getCamera().getFocus().getX());
-            mo.setY((int) Game.getCamera().getFocus().getY());
-            mo.setWidth((int) info.getWidth());
-            mo.setHeight((int) info.getHeight());
-            mo.setId(Game.getEnvironment().getNextMapId());
-            mo.setName("");
-            mo.setCustomProperty(MapObjectProperty.COLLISIONBOXWIDTH, (info.getWidth() * 0.4) + "");
-            mo.setCustomProperty(MapObjectProperty.COLLISIONBOXHEIGHT, (info.getHeight() * 0.4) + "");
-            mo.setCustomProperty(MapObjectProperty.COLLISION, "true");
-            mo.setCustomProperty(MapObjectProperty.INDESTRUCTIBLE, "false");
-            mo.setCustomProperty(MapObjectProperty.PROP_ADDSHADOW, "true");
-            mo.setCustomProperty(MapObjectProperty.SPRITESHEETNAME, propName);
-
-            EditorScreen.instance().getMapComponent().add(mo);
-            e.consume();
-          }
+        if (e.getClickCount() == 2 && addEntity()) {
+          e.consume();
         }
       }
     });
@@ -169,13 +141,25 @@ public class AssetPanelItem extends JPanel {
 
     this.setMinimumSize(new Dimension(this.iconLabel.getWidth(), this.iconLabel.getHeight() + this.textField.getHeight()));
 
-    JPanel panel = new JPanel();
-    panel.setPreferredSize(new Dimension(16, 20));
-    panel.setMinimumSize(new Dimension(16, 20));
+    JPanel panel = new JPanel(new GridLayout(1, 2, 0, 0));
+    panel.setPreferredSize(new Dimension(54, 20));
+    panel.setMinimumSize(new Dimension(54, 20));
     panel.setOpaque(false);
     add(panel, BorderLayout.EAST);
 
+    btnAdd = new JButton("");
+    btnAdd.setToolTipText("Add Entity");
+    btnAdd.addActionListener(e -> this.addEntity());
+    btnAdd.setMaximumSize(new Dimension(16, 16));
+    btnAdd.setMinimumSize(new Dimension(16, 16));
+    btnAdd.setPreferredSize(new Dimension(16, 16));
+    btnAdd.setOpaque(false);
+    btnAdd.setIcon(new ImageIcon(Resources.getImage("addx12.png")));
+    btnAdd.setVisible(false);
+    btnAdd.setEnabled(canAdd());
+
     btnEdit = new JButton("");
+    btnEdit.setToolTipText("Edit Asset");
     btnEdit.addActionListener(e -> {
       SpritesheetImportPanel spritePanel = new SpritesheetImportPanel((SpriteSheetInfo) this.getOrigin());
       int option = JOptionPane.showConfirmDialog(null, spritePanel, Resources.get("menu_assets_editSprite"), JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
@@ -193,11 +177,26 @@ public class AssetPanelItem extends JPanel {
       // asset to use the new name (assets are treated as reference by name)
       EditorScreen.instance().loadSpriteSheets(EditorScreen.instance().getGameFile().getSpriteSheets(), true);
     });
+    btnEdit.setMaximumSize(new Dimension(16, 16));
+    btnEdit.setMinimumSize(new Dimension(16, 16));
     btnEdit.setPreferredSize(new Dimension(16, 16));
     btnEdit.setOpaque(false);
     btnEdit.setIcon(new ImageIcon(Resources.getImage("pencil.png")));
     btnEdit.setVisible(false);
+
+    btnDelete = new JButton("");
+    btnDelete.setToolTipText("Delete Asset");
+    btnDelete.addActionListener(e -> this.deleteAsset());
+    btnDelete.setMaximumSize(new Dimension(16, 16));
+    btnDelete.setMinimumSize(new Dimension(16, 16));
+    btnDelete.setPreferredSize(new Dimension(16, 16));
+    btnDelete.setOpaque(false);
+    btnDelete.setIcon(new ImageIcon(Resources.getImage("button-deletex12.png")));
+    btnDelete.setVisible(false);
+
+    panel.add(btnAdd);
     panel.add(btnEdit);
+    panel.add(btnDelete);
   }
 
   public AssetPanelItem(Icon icon, String text, Object origin) {
@@ -209,5 +208,66 @@ public class AssetPanelItem extends JPanel {
 
   public Object getOrigin() {
     return this.origin;
+  }
+
+  private void deleteAsset() {
+    if (getOrigin() instanceof SpriteSheetInfo) {
+      SpriteSheetInfo info = (SpriteSheetInfo) getOrigin();
+      int n = JOptionPane.showConfirmDialog(null, "Do you really want to delete the spritesheet [" + info.getName() + "]?\n Entities that use the sprite won't be rendered anymore!", "Delete Spritesheet?", JOptionPane.YES_NO_OPTION);
+
+      if (n == JOptionPane.OK_OPTION) {
+        EditorScreen.instance().getGameFile().getSpriteSheets().remove(getOrigin());
+        ImageCache.clearAll();
+        Spritesheet.remove(info.getName());
+        EditorScreen.instance().getMapComponent().reloadEnvironment();
+
+        Program.getAssetTree().forceUpdate();
+      }
+    }
+  }
+
+  private boolean addEntity() {
+    // TODO: experimental code... this needs to be refactored with issue #66
+    if (this.getOrigin() instanceof SpriteSheetInfo) {
+      SpriteSheetInfo info = (SpriteSheetInfo) this.getOrigin();
+      String propName = Prop.getNameBySpriteName(info.getName());
+      if (propName == null) {
+        return false;
+      }
+
+      MapObject mo = new MapObject();
+      mo.setType(MapObjectType.PROP.name());
+      mo.setX((int) Game.getCamera().getFocus().getX());
+      mo.setY((int) Game.getCamera().getFocus().getY());
+      mo.setWidth((int) info.getWidth());
+      mo.setHeight((int) info.getHeight());
+      mo.setId(Game.getEnvironment().getNextMapId());
+      mo.setName("");
+      mo.setCustomProperty(MapObjectProperty.COLLISIONBOXWIDTH, (info.getWidth() * 0.4) + "");
+      mo.setCustomProperty(MapObjectProperty.COLLISIONBOXHEIGHT, (info.getHeight() * 0.4) + "");
+      mo.setCustomProperty(MapObjectProperty.COLLISION, "true");
+      mo.setCustomProperty(MapObjectProperty.INDESTRUCTIBLE, "false");
+      mo.setCustomProperty(MapObjectProperty.PROP_ADDSHADOW, "true");
+      mo.setCustomProperty(MapObjectProperty.SPRITESHEETNAME, propName);
+
+      EditorScreen.instance().getMapComponent().add(mo);
+      return true;
+    }
+
+    return false;
+  }
+
+  private boolean canAdd() {
+    if (this.getOrigin() != null && this.getOrigin() instanceof SpriteSheetInfo) {
+      SpriteSheetInfo info = (SpriteSheetInfo) this.getOrigin();
+      String propName = Prop.getNameBySpriteName(info.getName());
+      if (propName == null) {
+        return false;
+      }
+
+      return true;
+    }
+
+    return false;
   }
 }
