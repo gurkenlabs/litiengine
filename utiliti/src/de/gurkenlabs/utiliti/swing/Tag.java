0e71f6ca8d04ed3cea00b01d07562080aaeaeb76
Gurkenlabs <gurkenlabs@wibemedia.de>
2018-02-18 21:38:20 +0100

    Improve the new tag UI by hover and focus effects.

--- utiliti/src/de/gurkenlabs/utiliti/swing/Tag.java
+++ utiliti/src/de/gurkenlabs/utiliti/swing/Tag.java
@@ -5,6 +5,10 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Insets;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
@@ -15,9 +19,15 @@ import javax.swing.JPanel;
 import de.gurkenlabs.litiengine.Resources;
 
 public class Tag extends JPanel {
+  public static final BufferedImage DELETE_DISABLED = Resources.getImage("button-delete-disabledx7.png");
+  public static final BufferedImage DELETE = Resources.getImage("button-deletex7.png");
   public static final Color DEFAULT_TAG_COLOR = new Color(99, 113, 118);
+  public static final Color DEFAULT_TAG_HOVER_COLOR = DEFAULT_TAG_COLOR.darker();
   private JPanel panel;
   private JLabel lblText;
+  private JButton btnDelete;
+
+  private boolean deleteHovered;
 
   public Tag(String text) {
     this();
@@ -41,22 +51,74 @@ public class Tag extends JPanel {
     this.lblText.setFont(this.lblText.getFont().deriveFont(10f));
     this.panel.add(this.lblText);
 
-    JButton btnDelete = new JButton();
-    btnDelete.addActionListener(e -> {
+    this.addMouseListener(new MouseAdapter() {
+      @Override
+      public void mouseEntered(final MouseEvent e) {
+        panel.setBackground(DEFAULT_TAG_HOVER_COLOR);
+      }
+
+      @Override
+      public void mouseExited(final MouseEvent e) {
+        if (!deleteHovered) {
+          panel.setBackground(DEFAULT_TAG_COLOR);
+        }
+      }
+    });
+
+    this.btnDelete = new JButton();
+    this.btnDelete.addActionListener(e -> {
       final Container parent = this.getParent();
       parent.remove(this);
       parent.revalidate();
     });
-    btnDelete.setMargin(new Insets(2, 5, 2, 5));
-    btnDelete.setContentAreaFilled(false);
-    btnDelete.setBorderPainted(false);
-    btnDelete.setBorder(null);
-    btnDelete.setPreferredSize(new Dimension(7, 7));
-    BufferedImage img = Resources.getImage("button-deletex7.png");
-    if (img != null) {
-      btnDelete.setIcon(new ImageIcon(img));
+
+    this.btnDelete.addMouseListener(new MouseAdapter() {
+      @Override
+      public void mouseEntered(final MouseEvent e) {
+        if (DELETE != null) {
+          btnDelete.setIcon(new ImageIcon(DELETE));
+        }
+
+        panel.setBackground(DEFAULT_TAG_HOVER_COLOR);
+        deleteHovered = true;
+      }
+
+      @Override
+      public void mouseExited(final MouseEvent e) {
+        if (!btnDelete.hasFocus() && DELETE_DISABLED != null) {
+          btnDelete.setIcon(new ImageIcon(DELETE_DISABLED));
+        }
+
+        deleteHovered = false;
+      }
+    });
+
+    this.btnDelete.addFocusListener(new FocusListener() {
+      @Override
+      public void focusLost(FocusEvent e) {
+        if (DELETE_DISABLED != null) {
+          btnDelete.setIcon(new ImageIcon(DELETE_DISABLED));
+        }
+      }
+
+      @Override
+      public void focusGained(FocusEvent e) {
+        if (DELETE != null) {
+          btnDelete.setIcon(new ImageIcon(DELETE));
+        }
+      }
+    });
+
+    this.btnDelete.setMargin(new Insets(2, 5, 2, 5));
+    this.btnDelete.setContentAreaFilled(false);
+    this.btnDelete.setBorderPainted(false);
+    this.btnDelete.setFocusPainted(false);
+    this.btnDelete.setBorder(null);
+    this.btnDelete.setPreferredSize(new Dimension(7, 7));
+    if (DELETE_DISABLED != null) {
+      this.btnDelete.setIcon(new ImageIcon(DELETE_DISABLED));
     }
-    this.panel.add(btnDelete);
+    this.panel.add(this.btnDelete);
   }
 
   @Override
