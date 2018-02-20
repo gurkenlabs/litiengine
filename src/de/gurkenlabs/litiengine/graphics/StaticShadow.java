3f1ce8db4f15cb0089bec5f158b5da2d231ff580
Gurkenlabs <gurkenlabs@wibemedia.de>
2018-02-20 19:16:33 +0100

    Extract the shadow area creation from the StaticShadowLayer to the StaticShadow class.

--- src/de/gurkenlabs/litiengine/graphics/StaticShadow.java
+++ src/de/gurkenlabs/litiengine/graphics/StaticShadow.java
@@ -1,6 +1,9 @@
 package de.gurkenlabs.litiengine.graphics;
 
 import java.awt.Color;
+import java.awt.geom.Area;
+import java.awt.geom.Path2D;
+import java.awt.geom.Point2D;
 
 import de.gurkenlabs.litiengine.annotation.EntityInfo;
 import de.gurkenlabs.litiengine.entities.CollisionBox;
@@ -12,9 +15,11 @@ public class StaticShadow extends MapArea {
   public static final Color DEFAULT_COLOR = Color.BLACK;
   public static final int DEFAULT_OFFSET = 10;
 
+  private final CollisionBox origin;
+
   private StaticShadowType shadowType;
   private int shadowOffset;
-  private final CollisionBox origin;
+  private Area area;
 
   public StaticShadow(StaticShadowType shadowType) {
     this.shadowType = shadowType;
@@ -50,6 +55,37 @@ public class StaticShadow extends MapArea {
 
   public void setShadowType(final StaticShadowType shadowType) {
     this.shadowType = shadowType;
+    this.area = null;
+  }
+
+  @Override
+  public void setX(double x) {
+    super.setX(x);
+    this.area = null;
+  }
+
+  @Override
+  public void setY(double y) {
+    super.setY(y);
+    this.area = null;
+  }
+
+  @Override
+  public void setWidth(final float width) {
+    super.setWidth(width);
+    this.area = null;
+  }
+
+  @Override
+  public void setHeight(final float height) {
+    super.setHeight(height);
+    this.area = null;
+  }
+
+  @Override
+  public void setLocation(final Point2D location) {
+    super.setLocation(location);
+    this.area = null;
   }
 
   public CollisionBox getOrigin() {
@@ -65,11 +101,108 @@ public class StaticShadow extends MapArea {
     return "[" + this.getOrigin().toString() + "] -> " + super.toString();
   }
 
+  public Area getArea() {
+    if (this.getShadowType() == StaticShadowType.NONE) {
+      return null;
+    }
+    
+    if (this.area == null) {
+      this.createArea();
+    }
+    return this.area;
+  }
+
+  private void createArea() {
+    if (this.getShadowType() == StaticShadowType.NONE) {
+      return;
+    }
+
+    final Path2D parallelogram = new Path2D.Double();
+    if (shadowType.equals(StaticShadowType.DOWN)) {
+      parallelogram.moveTo(this.getX(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.lineTo(this.getX(), this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.closePath();
+    } else if (shadowType.equals(StaticShadowType.DOWNLEFT)) {
+      parallelogram.moveTo(this.getX(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY() + this.getHeight());
+      parallelogram.lineTo(this.getX() + this.getWidth() - this.getOffset() / 2.0, this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.lineTo(this.getX(), this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.closePath();
+    } else if (shadowType.equals(StaticShadowType.DOWNRIGHT)) {
+      parallelogram.moveTo(this.getX(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY() + this.getHeight());
+      parallelogram.lineTo(this.getX() + this.getWidth() + this.getOffset() / 2.0, this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.lineTo(this.getX(), this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.closePath();
+    } else if (shadowType.equals(StaticShadowType.LEFT)) {
+      parallelogram.moveTo(this.getX(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY() + this.getHeight());
+      parallelogram.lineTo(this.getX() + this.getWidth() - this.getOffset() / 2.0, this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.lineTo(this.getX() - this.getOffset() / 2.0, this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.lineTo(this.getX(), this.getY() + this.getHeight());
+      parallelogram.closePath();
+    } else if (shadowType.equals(StaticShadowType.LEFTDOWN)) {
+      parallelogram.moveTo(this.getX(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY() + this.getHeight());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.lineTo(this.getX() - this.getOffset() / 2.0, this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.lineTo(this.getX(), this.getY() + this.getHeight());
+      parallelogram.closePath();
+    } else if (shadowType.equals(StaticShadowType.LEFTRIGHT)) {
+      parallelogram.moveTo(this.getX(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY() + this.getHeight());
+      parallelogram.lineTo(this.getX() + this.getWidth() + this.getOffset() / 2.0, this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.lineTo(this.getX() - this.getOffset() / 2.0, this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.lineTo(this.getX(), this.getY() + this.getHeight());
+      parallelogram.closePath();
+    } else if (shadowType.equals(StaticShadowType.RIGHTLEFT)) {
+      parallelogram.moveTo(this.getX(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY() + this.getHeight());
+      parallelogram.lineTo(this.getX() + this.getWidth() - this.getOffset() / 2.0, this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.lineTo(this.getX() + this.getOffset() / 2.0, this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.lineTo(this.getX(), this.getY() + this.getHeight());
+      parallelogram.closePath();
+    } else if (shadowType.equals(StaticShadowType.RIGHT)) {
+      parallelogram.moveTo(this.getX(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY() + this.getHeight());
+      parallelogram.lineTo(this.getX() + this.getWidth() + this.getOffset() / 2.0, this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.lineTo(this.getX() + this.getOffset() / 2.0, this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.lineTo(this.getX(), this.getY() + this.getHeight());
+      parallelogram.closePath();
+    } else if (shadowType.equals(StaticShadowType.RIGHTDOWN)) {
+      parallelogram.moveTo(this.getX(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY() + this.getHeight());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.lineTo(this.getX() + this.getOffset() / 2.0, this.getY() + this.getHeight() + this.getOffset());
+      parallelogram.lineTo(this.getX(), this.getY() + this.getHeight());
+      parallelogram.closePath();
+    } else if (shadowType.equals(StaticShadowType.NOOFFSET)) {
+      parallelogram.moveTo(this.getX(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY());
+      parallelogram.lineTo(this.getX() + this.getWidth(), this.getY() + this.getHeight());
+      parallelogram.lineTo(this.getX(), this.getY() + this.getHeight());
+      parallelogram.closePath();
+    }
+
+    this.area = new Area(parallelogram);
+  }
+
   public int getOffset() {
     return shadowOffset;
   }
 
   public void setOffset(int shadowOffset) {
     this.shadowOffset = shadowOffset;
+    this.area = null;
   }
 }
--- src/de/gurkenlabs/litiengine/graphics/StaticShadowLayer.java
+++ src/de/gurkenlabs/litiengine/graphics/StaticShadowLayer.java
@@ -3,14 +3,10 @@ package de.gurkenlabs.litiengine.graphics;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Area;
-import java.awt.geom.Path2D;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.util.List;
 
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.environment.IEnvironment;
-import de.gurkenlabs.litiengine.graphics.StaticShadowType;
 import de.gurkenlabs.util.ImageProcessing;
 
 public class StaticShadowLayer extends ColorLayer implements IRenderable {
@@ -37,109 +33,22 @@ public class StaticShadowLayer extends ColorLayer implements IRenderable {
 
     final Color shadowColor = new Color(this.getColor().getRed(), this.getColor().getGreen(), this.getColor().getBlue(), this.getAlpha());
 
-    final List<Path2D> newStaticShadows = new ArrayList<>();
     // check if the collision boxes have shadows. if so, determine which
     // shadow is needed, create the shape and add it to the
     // list of static shadows.
-    for (final StaticShadow shadow : this.getEnvironment().getStaticShadows()) {
-      final double shadowX = shadow.getLocation().getX();
-      final double shadowY = shadow.getLocation().getY();
-      final double shadowWidth = shadow.getWidth();
-      final double shadowHeight = shadow.getHeight();
-
-      final StaticShadowType shadowType = shadow.getShadowType();
-
-      final Path2D parallelogram = new Path2D.Double();
-      if (shadowType.equals(StaticShadowType.DOWN)) {
-        parallelogram.moveTo(shadowX, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.lineTo(shadowX, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.closePath();
-      } else if (shadowType.equals(StaticShadowType.DOWNLEFT)) {
-        parallelogram.moveTo(shadowX, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY + shadowHeight);
-        parallelogram.lineTo(shadowX + shadowWidth - shadow.getOffset() / 2.0, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.lineTo(shadowX, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.closePath();
-      } else if (shadowType.equals(StaticShadowType.DOWNRIGHT)) {
-        parallelogram.moveTo(shadowX, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY + shadowHeight);
-        parallelogram.lineTo(shadowX + shadowWidth + shadow.getOffset() / 2.0, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.lineTo(shadowX, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.closePath();
-      } else if (shadowType.equals(StaticShadowType.LEFT)) {
-        parallelogram.moveTo(shadowX, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY + shadowHeight);
-        parallelogram.lineTo(shadowX + shadowWidth - shadow.getOffset() / 2.0, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.lineTo(shadowX - shadow.getOffset() / 2.0, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.lineTo(shadowX, shadowY + shadowHeight);
-        parallelogram.closePath();
-      } else if (shadowType.equals(StaticShadowType.LEFTDOWN)) {
-        parallelogram.moveTo(shadowX, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY + shadowHeight);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.lineTo(shadowX - shadow.getOffset() / 2.0, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.lineTo(shadowX, shadowY + shadowHeight);
-        parallelogram.closePath();
-      } else if (shadowType.equals(StaticShadowType.LEFTRIGHT)) {
-        parallelogram.moveTo(shadowX, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY + shadowHeight);
-        parallelogram.lineTo(shadowX + shadowWidth + shadow.getOffset() / 2.0, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.lineTo(shadowX - shadow.getOffset() / 2.0, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.lineTo(shadowX, shadowY + shadowHeight);
-        parallelogram.closePath();
-      } else if (shadowType.equals(StaticShadowType.RIGHTLEFT)) {
-        parallelogram.moveTo(shadowX, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY + shadowHeight);
-        parallelogram.lineTo(shadowX + shadowWidth - shadow.getOffset() / 2.0, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.lineTo(shadowX + shadow.getOffset() / 2.0, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.lineTo(shadowX, shadowY + shadowHeight);
-        parallelogram.closePath();
-      } else if (shadowType.equals(StaticShadowType.RIGHT)) {
-        parallelogram.moveTo(shadowX, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY + shadowHeight);
-        parallelogram.lineTo(shadowX + shadowWidth + shadow.getOffset() / 2.0, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.lineTo(shadowX + shadow.getOffset() / 2.0, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.lineTo(shadowX, shadowY + shadowHeight);
-        parallelogram.closePath();
-      } else if (shadowType.equals(StaticShadowType.RIGHTDOWN)) {
-        parallelogram.moveTo(shadowX, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY + shadowHeight);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.lineTo(shadowX + shadow.getOffset() / 2.0, shadowY + shadowHeight + shadow.getOffset());
-        parallelogram.lineTo(shadowX, shadowY + shadowHeight);
-        parallelogram.closePath();
-      } else if (shadowType.equals(StaticShadowType.NOOFFSET)) {
-        parallelogram.moveTo(shadowX, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY);
-        parallelogram.lineTo(shadowX + shadowWidth, shadowY + shadowHeight);
-        parallelogram.lineTo(shadowX, shadowY + shadowHeight);
-        parallelogram.closePath();
-      }
-
-      if (parallelogram.getWindingRule() != 0) {
-        newStaticShadows.add(parallelogram);
-      }
-    }
-
     final BufferedImage img = ImageProcessing.getCompatibleImage((int) this.getEnvironment().getMap().getSizeInPixels().getWidth(), (int) this.getEnvironment().getMap().getSizeInPixels().getHeight());
     final Graphics2D g = img.createGraphics();
     g.setColor(shadowColor);
 
     final Area ar = new Area();
-    for (final Path2D staticShadow : newStaticShadows) {
-      final Area staticShadowArea = new Area(staticShadow);
+    for (final StaticShadow staticShadow : this.getEnvironment().getStaticShadows()) {
+      if(staticShadow.getShadowType() == StaticShadowType.NONE) {
+        continue;
+      }
+      
+      final Area staticShadowArea = staticShadow.getArea();
       for (final LightSource light : this.getEnvironment().getLightSources()) {
-        if (light.getDimensionCenter().getY() > staticShadow.getBounds2D().getMaxY() || staticShadow.getBounds2D().contains(light.getDimensionCenter())) {
+        if (light.getDimensionCenter().getY() > staticShadow.getBoundingBox().getMaxY() || staticShadow.getBoundingBox().contains(light.getDimensionCenter())) {
           staticShadowArea.subtract(new Area(light.getLightShape()));
         }
       }
