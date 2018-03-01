b4e47ef4ae931c9815835c3cb5204e1062a9de51
Gurkenlabs <gurkenlabs@wibemedia.de>
2018-03-01 16:00:05 +0100

    Implemented a mechanism that allow to annotate custom mapobject properties on a custom entity implementation.
    This will automatically be loaded by the base MapObjectLoader via reflection. There just needs to be a appropriate member field that is not final.
    
    Extracted a ton of reflection logic to a separate ReflectionUtilities class.

--- src/de/gurkenlabs/litiengine/environment/MapObjectLoader.java
+++ src/de/gurkenlabs/litiengine/environment/MapObjectLoader.java
@@ -1,15 +1,18 @@
 package de.gurkenlabs.litiengine.environment;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collection;
 
 import de.gurkenlabs.litiengine.Align;
 import de.gurkenlabs.litiengine.Valign;
+import de.gurkenlabs.litiengine.annotation.CustomMapObjectProperty;
 import de.gurkenlabs.litiengine.entities.ICollisionEntity;
 import de.gurkenlabs.litiengine.entities.IEntity;
 import de.gurkenlabs.litiengine.environment.tilemap.IMapObject;
 import de.gurkenlabs.litiengine.environment.tilemap.MapObjectProperty;
 import de.gurkenlabs.litiengine.environment.tilemap.MapObjectType;
+import de.gurkenlabs.litiengine.util.ReflectionUtilities;
 
 public abstract class MapObjectLoader implements IMapObjectLoader {
   private final String mapObjectType;
@@ -26,7 +29,7 @@ public abstract class MapObjectLoader implements IMapObjectLoader {
   public String getMapObjectType() {
     return this.mapObjectType;
   }
-  
+
   @Override
   public Collection<IEntity> load(IEnvironment environment, IMapObject mapObject) {
     return new ArrayList<>();
@@ -52,6 +55,24 @@ public abstract class MapObjectLoader implements IMapObjectLoader {
         entity.addTag(tag);
       }
     }
+
+    loadCustomMapObjectProperties(entity, mapObject);
+  }
+
+  private static void loadCustomMapObjectProperties(IEntity entity, IMapObject mapObject) {
+    CustomMapObjectProperty customProp = entity.getClass().getAnnotation(CustomMapObjectProperty.class);
+    if (customProp == null) {
+      return;
+    }
+
+    final String memberField = customProp.memberField() != null && !customProp.memberField().isEmpty() ? customProp.memberField() : customProp.key();
+
+    Field field = ReflectionUtilities.getField(entity.getClass(), memberField);
+    if (field == null) {
+      return;
+    }
+
+    ReflectionUtilities.setFieldValue(entity.getClass(), entity, memberField, mapObject.getCustomProperty(customProp.key()));
   }
 
   public static void loadCollisionProperties(ICollisionEntity entity, IMapObject mapObject) {
