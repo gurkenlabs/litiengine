a6730d75007742b54ce27f6d5479b5eff7349a3c
Gurkenlabs <gurkenlabs@wibemedia.de>
2018-04-28 14:22:29 +0200

    Add javadoc for trigger listeners.

--- src/de/gurkenlabs/litiengine/entities/TriggerActivatedListener.java
+++ src/de/gurkenlabs/litiengine/entities/TriggerActivatedListener.java
@@ -2,6 +2,18 @@ package de.gurkenlabs.litiengine.entities;
 
 import java.util.EventListener;
 
+/**
+ * This listener provides a callback for when a <code>Trigger</code> was activated.
+ */
 public interface TriggerActivatedListener extends EventListener {
+  
+  /**
+   * This method is called when a <code>Trigger</code> was activated.
+   * 
+   * @param event
+   *          The event data that contains information about the trigger.
+   *          
+   * @see Trigger#isActivated()
+   */
   public void activated(TriggerEvent event);
 }
--- src/de/gurkenlabs/litiengine/entities/TriggerListener.java
+++ src/de/gurkenlabs/litiengine/entities/TriggerListener.java
@@ -1,4 +1,7 @@
 package de.gurkenlabs.litiengine.entities;
 
+/**
+ * This listener provides callbacks for when a <code>Trigger</code> gets activated or deactivated.
+ */
 public interface TriggerListener extends TriggerActivatedListener, TriggerDeactivatedListener, TriggerActivatingCondition {
 }
