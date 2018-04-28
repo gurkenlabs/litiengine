a37556711acde9031bf68a6321b6c2b9f838ff4c
Gurkenlabs <gurkenlabs@wibemedia.de>
2018-04-28 13:57:00 +0200

    Add documentation to EntityTransformListener

--- src/de/gurkenlabs/litiengine/entities/EntityTransformAdapter.java
+++ src/de/gurkenlabs/litiengine/entities/EntityTransformAdapter.java
@@ -1,5 +1,11 @@
 package de.gurkenlabs.litiengine.entities;
 
+/**
+ * An abstract implementation of a <code>EntityTransformListener</code> that allows to only overwrite
+ * individual callbacks in anonymous implementations.
+ * 
+ * @see EntityTransformListener
+ */
 public abstract class EntityTransformAdapter implements EntityTransformListener {
 
   @Override
--- src/de/gurkenlabs/litiengine/entities/EntityTransformListener.java
+++ src/de/gurkenlabs/litiengine/entities/EntityTransformListener.java
@@ -2,8 +2,33 @@ package de.gurkenlabs.litiengine.entities;
 
 import java.util.EventListener;
 
+/**
+ * This listener provides callbacks for when an <code>Entity</code> was moved or changed its size.
+ */
 public interface EntityTransformListener extends EventListener {
+
+  /**
+   * This method is called whenever the location of an <code>IEntity</code> was changed.
+   * 
+   * @param entity
+   *          The entity that changed its location.
+   * 
+   * @see IEntity#setLocation(java.awt.geom.Point2D)
+   * @see IEntity#setLocation(double, double)
+   * @see IEntity#setX(double)
+   * @see IEntity#setY(double)
+   */
   public void locationChanged(IEntity entity);
 
+  /**
+   * This method is called whenever the size of an <code>IEntity</code> was changed.
+   * 
+   * @param entity
+   *          The entity that changed its size.
+   *          
+   * @see IEntity#setSize(float, float)
+   * @see IEntity#setHeight(float)
+   * @see IEntity#setWidth(float)
+   */
   public void sizeChanged(IEntity entity);
 }
