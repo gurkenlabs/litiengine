c5d663d4584cd5a968057f9eef9bc6085fe93a50
Gurkenlabs <gurkenlabs@wibemedia.de>
2018-06-05 21:01:10 +0200

    Elevate version to v0.4.12-alpha

--- utiliti/src/de/gurkenlabs/utiliti/Program.java
+++ utiliti/src/de/gurkenlabs/utiliti/Program.java
@@ -112,7 +112,7 @@ public class Program {
 
     Game.getInfo().setName("utiLITI");
     Game.getInfo().setSubTitle("litiengine creation kit");
-    Game.getInfo().setVersion("v0.4.11-alpha");
+    Game.getInfo().setVersion("v0.4.12-alpha");
 
     initSystemTray();
 
