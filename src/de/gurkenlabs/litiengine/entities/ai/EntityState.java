7524a29465f844eec10463e8febef61db23a2b56
TheRamenChef <bananaweeklyguy@gmail.com>
2018-06-19 13:58:44 -0500

    Update EntityState.java
    Remove IEnvironment and start removing the interfaces in this package

--- src/de/gurkenlabs/litiengine/entities/ai/EntityState.java
+++ src/de/gurkenlabs/litiengine/entities/ai/EntityState.java
@@ -1,25 +1,24 @@
 package de.gurkenlabs.litiengine.entities.ai;
 
 import de.gurkenlabs.litiengine.entities.Entity;
-import de.gurkenlabs.litiengine.environment.IEnvironment;
+import de.gurkenlabs.litiengine.environment.Environment;
 import de.gurkenlabs.litiengine.states.State;
 
-public abstract class EntityState<T extends Entity> extends State implements IEntityState {
+public abstract class EntityState<T extends Entity> extends State {
   private final T entity;
-  private final IEnvironment environment;
+  private final Environment environment;
 
-  protected EntityState(final String name, final T entity, final IEnvironment env) {
+  protected EntityState(final String name, final T entity, final Environment env) {
     super(name);
     this.entity = entity;
     this.environment = env;
   }
 
-  @Override
   public T getEntity() {
     return this.entity;
   }
 
-  public IEnvironment getEnvironment() {
+  public Environment getEnvironment() {
     return this.environment;
   }
 }
