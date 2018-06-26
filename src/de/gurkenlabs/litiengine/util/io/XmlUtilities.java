aa65bc243add6aea9d1473d54aca88c4e1852e07
TheRamenChef <bananaweeklyguy@gmail.com>
2018-06-26 11:24:54 -0500

    Fix a typo

--- src/de/gurkenlabs/litiengine/util/io/XmlUtilities.java
+++ src/de/gurkenlabs/litiengine/util/io/XmlUtilities.java
@@ -50,7 +50,7 @@ public final class XmlUtilities {
    * @param indentation
    *          The indentation with which the XML should be saved.
    */
-  public static void saveWithCustomIndetation(ByteArrayInputStream input, FileOutputStream fos, int indentation) {
+  public static void saveWithCustomIndentation(ByteArrayInputStream input, FileOutputStream fos, int indentation) {
     try {
       Transformer transformer = SAXTransformerFactory.newInstance().newTransformer();
       transformer.setOutputProperty(OutputKeys.DOCTYPE_PUBLIC, "yes");
@@ -128,7 +128,7 @@ public final class XmlUtilities {
       out.flush();
 
       // second: postprocess xml and then write it to the file
-      XmlUtilities.saveWithCustomIndetation(new ByteArrayInputStream(out.toByteArray()), fileOut, 1);
+      XmlUtilities.saveWithCustomIndentation(new ByteArrayInputStream(out.toByteArray()), fileOut, 1);
       out.close();
 
       jaxbMarshaller.marshal(object, out);
