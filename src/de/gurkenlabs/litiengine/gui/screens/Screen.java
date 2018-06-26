210ee0bc1e47ff43c51abfc2ac8163d3bb8fe9a9
TheRamenChef <bananaweeklyguy@gmail.com>
2018-06-26 12:47:33 -0500

    Remove ScreenInfo; it's a poor implementation

--- src/de/gurkenlabs/litiengine/gui/screens/Screen.java
+++ src/de/gurkenlabs/litiengine/gui/screens/Screen.java
@@ -1,22 +1,9 @@
 package de.gurkenlabs.litiengine.gui.screens;
 
-import java.lang.annotation.AnnotationFormatError;
-
-import de.gurkenlabs.litiengine.annotation.ScreenInfo;
 import de.gurkenlabs.litiengine.gui.GuiComponent;
 
 public abstract class Screen extends GuiComponent implements IScreen {
   private final String name;
-
-  protected Screen() {
-    super(0, 0);
-    final ScreenInfo info = this.getClass().getAnnotation(ScreenInfo.class);
-    if (info == null) {
-      throw new AnnotationFormatError("No ScreenInfo annotation found on screen " + this.getClass());
-    }
-
-    this.name = info.name();
-  }
 
   protected Screen(final String screenName) {
     super(0, 0);
