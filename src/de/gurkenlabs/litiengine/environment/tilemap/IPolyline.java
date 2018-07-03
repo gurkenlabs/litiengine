ad65abb8f1f688d8662b18b33b015c3d88d53079
TheRamenChef <bananaweeklyguy@gmail.com>
2018-07-03 15:00:33 -0500

    Fix yet another typo

--- src/de/gurkenlabs/litiengine/environment/tilemap/IPolyline.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/IPolyline.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface IPolyline {
   /**
    * Gets all points of a polyline. The points are relative to the x and y
-   * coordiante of the parent {@link IMapObject}.
+   * coordinate of the parent {@link IMapObject}.
    * 
    * @return A list containing all points of the polyline.
    */
