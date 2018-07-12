6b0b21f3567ba8def0ba88191f55b21c14813537
TheRamenChef <bananaweeklyguy@gmail.com>
2018-07-11 19:14:07 -0500

    The game should crash if an exception isn't caught.

--- src/de/gurkenlabs/litiengine/DefaultUncaughtExceptionHandler.java
+++ src/de/gurkenlabs/litiengine/DefaultUncaughtExceptionHandler.java
@@ -1,8 +1,9 @@
 package de.gurkenlabs.litiengine;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.io.IOException;
 import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DefaultUncaughtExceptionHandler implements UncaughtExceptionHandler {
@@ -10,9 +11,16 @@ public class DefaultUncaughtExceptionHandler implements UncaughtExceptionHandler
 
   @Override
   public void uncaughtException(final Thread t, final Throwable e) {
-    final StringWriter sw = new StringWriter();
-    e.printStackTrace(new PrintWriter(sw));
-    final String stacktrace = sw.toString();
-    log.severe(stacktrace);
+    if (e instanceof ThreadDeath)
+      return;
+    
+    try {
+      log.addHandler(new FileHandler("crash.txt"));
+    } catch (SecurityException | IOException e2) {
+      log.log(Level.WARNING, "Could not create crash report file.", e);
+    }
+    
+    log.log(Level.SEVERE, String.format("Game crashed! :(%n%s threw an exception:%n", t.getName()), e);
+    System.exit(Game.EXIT_GAME_CRASHED);
   }
 }
