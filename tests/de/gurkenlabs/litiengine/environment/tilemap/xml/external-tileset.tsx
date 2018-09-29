b5a8de510e5e151318bf2f338973b44bd5fb1293
Steffen Wilke <mail@steffen-wilke.de>
2018-09-29 13:03:20 +0200

    Fix tileset width.

--- tests/de/gurkenlabs/litiengine/environment/tilemap/xml/external-tileset.tsx
+++ tests/de/gurkenlabs/litiengine/environment/tilemap/xml/external-tileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="tiles-test" tilewidth="16" tileheight="16" tilecount="2" columns="2">
- <image source="tiles-test.png" trans="ff00ff" width="16" height="16"/>
+ <image source="tiles-test.png" trans="ff00ff" width="32" height="16"/>
  <tile id="0">
   <properties>
    <property name="baz" value="bap"/>
