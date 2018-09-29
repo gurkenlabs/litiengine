dfba899334ba42875c35863525fecd19d2fae5f3
Steffen Wilke <mail@steffen-wilke.de>
2018-09-29 13:04:37 +0200

    Adjust naming of external tileset.

--- tests/de/gurkenlabs/litiengine/environment/tilemap/xml/external-tileset.tsx
+++ tests/de/gurkenlabs/litiengine/environment/tilemap/xml/external-tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tiles-test" tilewidth="16" tileheight="16" tilecount="2" columns="2">
+<tileset name="external-tileset" tilewidth="16" tileheight="16" tilecount="2" columns="2">
  <image source="tiles-test.png" trans="ff00ff" width="32" height="16"/>
  <tile id="0">
   <properties>
