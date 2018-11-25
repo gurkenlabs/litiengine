0f80351da1e0c8a05e5ecfc8f254f4c95524533e
Steffen Wilke <mail@steffen-wilke.de>
2018-11-25 19:34:50 +0100

    Add Javadoc to Resources class.

--- src/de/gurkenlabs/litiengine/resources/Resources.java
+++ src/de/gurkenlabs/litiengine/resources/Resources.java
@@ -1,5 +1,30 @@
 package de.gurkenlabs.litiengine.resources;
 
+import java.awt.Font;
+import java.awt.image.BufferedImage;
+
+import de.gurkenlabs.litiengine.environment.tilemap.IMap;
+import de.gurkenlabs.litiengine.graphics.Spritesheet;
+import de.gurkenlabs.litiengine.sound.Sound;
+
+/**
+ * This class is the engines entry point for accessing any kind of resources. A resource is any non-executable data that is deployed with your game.
+ * The <code>Resources</code> class provides access to types of <code>ResourcesContainers</code> and is used by different (loading) mechanisms to make
+ * resources available during runtime.
+ * <p>
+ * The LITIengine supports a variety of different resource types, including:
+ * <ul>
+ * <li>images</li>
+ * <li>fonts</li>
+ * <li>maps</li>
+ * <li>(localizable) strings</li>
+ * <li>spritesheets</li>
+ * <li>sounds</li>
+ * </ul>
+ * </p>
+ * 
+ * @see ResourcesContainer
+ */
 public final class Resources {
   private static Fonts fonts;
   private static Sounds sounds;
@@ -20,26 +45,67 @@ public final class Resources {
   private Resources() {
   }
 
+  /**
+   * Gets the container that manages <code>Font</code> resources.
+   * 
+   * @return The Font resource container.
+   * 
+   * @see Font
+   */
   public static Fonts fonts() {
     return fonts;
   }
 
+  /**
+   * Gets the container that manages <code>Sound</code> resources.
+   * 
+   * @return The Sound resource container.
+   * 
+   * @see Sound
+   */
   public static Sounds sounds() {
     return sounds;
   }
 
+  /**
+   * Gets the container that manages <code>IMap</code> resources.
+   * 
+   * @return The IMap resource container.
+   * 
+   * @see IMap
+   */
   public static Maps maps() {
     return maps;
   }
 
+  /**
+   * Gets a container that manages <code>String</code> resources.
+   * This instance can be used to access localizable string from a ".properties" file.
+   * 
+   * @return The String resource container.
+   */
   public static Strings strings() {
     return strings;
   }
 
+  /**
+   * Gets the container that manages <code>BufferedImage</code> resources.
+   * 
+   * @return The BufferedImage resource container.
+   * 
+   * @see BufferedImage
+   */
   public static Images images() {
     return images;
   }
 
+  /**
+   * Gets the container that manages <code>Spritesheet</code> resources.
+   * 
+   * @return The Spritesheet resource container.
+   * 
+   * @see Spritesheet
+   */
   public static Spritesheets spritesheets() {
     return spritesheets;
   }
