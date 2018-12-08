7ec9d82c166c311bff4d464587b2d727cdd35d63
Steffen Wilke <mail@steffen-wilke.de>
2018-12-08 13:55:43 +0100

    Improve javadocs on game configuration. Extract gamepad input configuration to the InputConfiguration instead of being static. With this change, it's possible to configure the deadzone for different buttons and axis on a controller. Make it possible to adjust the deadzones per Gamepad.

--- src/de/gurkenlabs/litiengine/configuration/InputConfiguration.java
+++ src/de/gurkenlabs/litiengine/configuration/InputConfiguration.java
@@ -4,25 +4,55 @@ package de.gurkenlabs.litiengine.configuration;
 public class InputConfiguration extends ConfigurationGroup {
   private float mouseSensitivity;
   private boolean gamepadSupport;
+  private float gamepadAxisDeadzone;
+  private float gamepadTriggerDeadzone;
+  private float gamepadStickDeadzone;
 
   public InputConfiguration() {
     this.setMouseSensitivity(1.0F);
     this.setGamepadSupport(false);
+    this.setGamepadAxisDeadzone(0.3f);
+    this.setGamepadTriggerDeadzone(0.1f);
+    this.setGamepadStickDeadzone(0.15f);
   }
 
   public float getMouseSensitivity() {
     return this.mouseSensitivity;
   }
 
-  public void setMouseSensitivity(final float mouseSensitivity) {
-    this.mouseSensitivity = mouseSensitivity;
+  public float getGamepadAxisDeadzone() {
+    return this.gamepadAxisDeadzone;
+  }
+
+  public float getGamepadTriggerDeadzone() {
+    return this.gamepadTriggerDeadzone;
+  }
+
+  public float getGamepadStickDeadzone() {
+    return this.gamepadStickDeadzone;
   }
 
   public boolean isGamepadSupport() {
     return gamepadSupport;
   }
 
+  public void setMouseSensitivity(final float mouseSensitivity) {
+    this.mouseSensitivity = mouseSensitivity;
+  }
+
   public void setGamepadSupport(boolean gamepadSupport) {
     this.gamepadSupport = gamepadSupport;
+  }
+
+  public void setGamepadAxisDeadzone(float gamepadAxisDeadzone) {
+    this.gamepadAxisDeadzone = gamepadAxisDeadzone;
+  }
+
+  public void setGamepadTriggerDeadzone(float gamepadTriggerDeadzone) {
+    this.gamepadTriggerDeadzone = gamepadTriggerDeadzone;
+  }
+
+  public void setGamepadStickDeadzone(float gamepadStickDeadzone) {
+    this.gamepadStickDeadzone = gamepadStickDeadzone;
   }
 }
--- tests/de/gurkenlabs/litiengine/GameTest.java
+++ tests/de/gurkenlabs/litiengine/GameTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.File;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GameTest {
