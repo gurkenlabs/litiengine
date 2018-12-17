202ac7a2ebfd02938bea64e5bdc668d6982cc988
TheRamenChef <bananaweeklyguy@gmail.com>
2018-12-17 13:35:34 -0600

    Synchronize these methods
    If two threads call the `init` method without proper synchronization, the two threads could get different instances of each `Game` utility, and that could cause very bad things to happen.

--- src/de/gurkenlabs/litiengine/Game.java
+++ src/de/gurkenlabs/litiengine/Game.java
@@ -498,7 +498,7 @@ public final class Game {
    * @param args
    *          The arguments passed to the programs entry point.
    */
-  public static void init(String... args) {
+  public synchronized static void init(String... args) {
     if (initialized) {
       log.log(Level.INFO, "The game has already been initialized.");
       return;
@@ -588,7 +588,7 @@ public final class Game {
    * @see Input
    * @see GameListener#started()
    */
-  public static void start() {
+  public synchronized static void start() {
     if (!initialized) {
       throw new IllegalStateException("The game cannot be started without being first initialized. Call Game.init(...) before Game.start().");
     }
@@ -612,7 +612,7 @@ public final class Game {
    * This method <b>should not be called manually</b>. <br>
    * It's internally called when the main application shuts down.
    */
-  public static void terminate() {
+  public synchronized static void terminate() {
     for (final GameListener listener : gameListeners) {
       if (!listener.terminating()) {
         return;
