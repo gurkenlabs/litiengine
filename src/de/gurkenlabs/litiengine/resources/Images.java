8cfad6d94f6b4e7fbce27e418fd4cd3fdbbe4285
TheRamenChef <bananaweeklyguy@gmail.com>
2018-12-17 19:35:54 -0600

    ImageIO.read already checks for support

--- src/de/gurkenlabs/litiengine/resources/Images.java
+++ src/de/gurkenlabs/litiengine/resources/Images.java
@@ -3,23 +3,18 @@ package de.gurkenlabs.litiengine.resources;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
 
-import de.gurkenlabs.litiengine.graphics.ImageFormat;
 import de.gurkenlabs.litiengine.util.ImageProcessing;
 import de.gurkenlabs.litiengine.util.io.FileUtilities;
 
 public final class Images extends ResourcesContainer<BufferedImage> {
-  private static final Logger log = Logger.getLogger(Images.class.getName());
-
   Images() {
   }
 
   /**
-   * Loads the image by the specified resourceName. This method supports both,
+   * Loads the image by the specified resourceName. This method supports both
    * loading images from a folder and loading them from the resources.
    *
    * @param resourceName
@@ -30,11 +25,6 @@ public final class Images extends ResourcesContainer<BufferedImage> {
   @Override
   protected BufferedImage load(String resourceName) throws IOException {
     if (resourceName == null || resourceName.isEmpty()) {
-      return null;
-    }
-
-    if (!ImageFormat.isSupported(resourceName)) {
-      log.log(Level.SEVERE, "The image file {0} could not be loaded because the image format is not supported.", new Object[] { resourceName });
       return null;
     }
 
