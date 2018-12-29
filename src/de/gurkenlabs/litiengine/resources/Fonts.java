552ba82a47a5e328949be15c0d6bf4a5b81e3291
Steffen Wilke <mail@steffen-wilke.de>
2018-12-29 14:07:16 +0100

    Improve font loading performance
    Since custom fonts should be accessed via Resources.fonts(), there is no need to register the font on the graphics environment.

--- src/de/gurkenlabs/litiengine/resources/Fonts.java
+++ src/de/gurkenlabs/litiengine/resources/Fonts.java
@@ -2,7 +2,6 @@ package de.gurkenlabs.litiengine.resources;
 
 import java.awt.Font;
 import java.awt.FontFormatException;
-import java.awt.GraphicsEnvironment;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Level;
@@ -17,31 +16,26 @@ public final class Fonts extends ResourcesContainer<Font> {
   }
 
   /***
-   * Loads a custom font with the specified name from game's resources and registers it on the <code>GraphicsEnvironment</code>.
+   * Loads a custom font with the specified name from game's resources.
    * As a fallback, when no font could be found by the specified <code>fontName</code>, it tries to get the font from the environment by calling.
    *
    * @param resourceName
    *          The name of the font
    * @return The loaded font.
    * 
-   * @see GraphicsEnvironment#registerFont(Font)
    * @see Font#createFont(int, java.io.File)
    * @see Font#getFont(String)
    */
   @Override
   protected Font load(String resourceName) {
     try {
-      final GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-
       final InputStream fontStream = FileUtilities.getGameResource(resourceName);
       if (fontStream == null) {
         log.log(Level.SEVERE, "font {0} could not be loaded", resourceName);
         return null;
       }
 
-      final Font font = Font.createFont(Font.TRUETYPE_FONT, fontStream);
-      ge.registerFont(font);
-      return font;
+      return Font.createFont(Font.TRUETYPE_FONT, fontStream);
     } catch (final FontFormatException | IOException e) {
       log.log(Level.SEVERE, e.getMessage(), e);
     }
