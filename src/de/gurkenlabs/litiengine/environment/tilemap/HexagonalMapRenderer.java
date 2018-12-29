8e2cd2a704f12b5a3babac117768194b78cc8201
Steffen Wilke <mail@steffen-wilke.de>
2018-12-29 16:19:35 +0100

    DRY
    Don't copy stuff, it's bad!

--- src/de/gurkenlabs/litiengine/environment/tilemap/HexagonalMapRenderer.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/HexagonalMapRenderer.java
@@ -9,162 +9,15 @@ import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.util.Optional;
 
-import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.graphics.ImageRenderer;
-import de.gurkenlabs.litiengine.graphics.RenderType;
-import de.gurkenlabs.litiengine.graphics.Spritesheet;
 import de.gurkenlabs.litiengine.resources.Resources;
 import de.gurkenlabs.litiengine.util.ImageProcessing;
 
-public class HexagonalMapRenderer implements IMapRenderer {
-  @Override
-  public BufferedImage getImage(IMap map, RenderType... renderTypes) {
-    final String cacheKey = getCacheKey(map) + "_" + renderTypes;
-    Optional<BufferedImage> opt = Resources.images().tryGet(cacheKey);
-    if (opt.isPresent()) {
-      return opt.get();
-    }
-
-    final BufferedImage img = ImageProcessing.getCompatibleImage((int) map.getSizeInPixels().getWidth(), (int) map.getSizeInPixels().getHeight());
-    final Graphics2D g = img.createGraphics();
-
-    for (final ITileLayer layer : map.getTileLayers()) {
-      if (layer == null || !shouldBeRendered(layer, renderTypes)) {
-        continue;
-      }
-
-      ImageRenderer.render(g, this.getLayerImage(layer, map, true), layer.getOffset());
-    }
-
-    g.dispose();
-
-    Resources.images().add(cacheKey, img);
-    return img;
-  }
-
-  @Override
-  public BufferedImage getImage(IMap map) {
-    return this.getImage(map, RenderType.BACKGROUND, RenderType.GROUND, RenderType.SURFACE, RenderType.NORMAL, RenderType.OVERLAY);
-  }
+public class HexagonalMapRenderer extends MapRenderer {
 
   @Override
   public MapOrientation getSupportedOrientation() {
     return MapOrientation.HEXAGONAL;
-  }
-
-  @Override
-  public void render(final Graphics2D g, final IMap map, RenderType... renderTypes) {
-    this.render(g, map, 0, 0, renderTypes);
-  }
-
-  @Override
-  public void render(final Graphics2D g, final IMap map, final double offsetX, final double offsetY, RenderType... renderTypes) {
-    final BufferedImage mapImage = this.getImage(map, renderTypes);
-    ImageRenderer.render(g, mapImage, offsetX, offsetY);
-  }
-
-  @Override
-  public void render(final Graphics2D g, final IMap map, final Rectangle2D viewport, RenderType... renderTypes) {
-    for (final ILayer layer : map.getRenderLayers()) {
-      if (layer == null || !shouldBeRendered(layer, renderTypes)) {
-        continue;
-      }
-
-      if (layer instanceof ITileLayer) {
-        this.renderTileLayerImage(g, (ITileLayer) layer, map, viewport);
-      }
-
-      if (layer instanceof IImageLayer) {
-        this.renderImageLayer(g, (IImageLayer) layer, viewport);
-      }
-    }
-  }
-
-  private static boolean shouldBeRendered(ILayer layer, RenderType[] renderTypes) {
-    if (renderTypes == null || renderTypes.length == 0) {
-      return isVisible(layer);
-    }
-
-    for (RenderType alloc : renderTypes) {
-      if (alloc == layer.getRenderType()) {
-        return isVisible(layer);
-      }
-    }
-
-    return false;
-  }
-
-  private static boolean isVisible(ILayer layer) {
-    return layer.isVisible() && layer.getOpacity() > 0;
-  }
-
-  /**
-   * Gets the cache key.
-   *
-   * @param map
-   *          the map
-   * @return the cache key
-   */
-  private static String getCacheKey(final IMap map) {
-    return "map_" + map.getName();
-  }
-
-  private static Image getTileImage(final IMap map, final ITile tile) {
-    if (tile == null) {
-      return null;
-    }
-
-    final ITileset tileset = MapUtilities.findTileSet(map, tile);
-    if (tileset == null || tileset.getFirstGridId() > tile.getGridId()) {
-      return null;
-    }
-
-    Spritesheet sprite = tileset.getSpritesheet();
-    if (sprite == null) {
-      return null;
-    }
-
-    // get the grid id relative to the sprite sheet since we use a 0 based
-    // approach to calculate the position
-    int index = tile.getGridId() - tileset.getFirstGridId();
-
-    // support for animated tiles
-    final ITileAnimation animation = MapUtilities.getAnimation(map, index);
-    if (animation != null && !animation.getFrames().isEmpty()) {
-      final long playedMs = Game.time().sinceGameStart();
-
-      final int totalDuration = animation.getTotalDuration();
-      final long animationsPlayed = playedMs / totalDuration;
-
-      final long deltaTicks = playedMs - animationsPlayed * totalDuration;
-      int currentPlayTime = 0;
-      for (final ITileAnimationFrame frame : animation.getFrames()) {
-        currentPlayTime += frame.getDuration();
-        if (deltaTicks < currentPlayTime) {
-          // found the current animation frame
-          index = frame.getTileId();
-          break;
-        }
-      }
-    }
-
-    BufferedImage tileImage = sprite.getSprite(index, tileset.getMargin(), tileset.getSpacing());
-    if (tile.isFlipped()) {
-      if (tile.isFlippedDiagonally()) {
-        tileImage = ImageProcessing.rotate(tileImage, Math.toRadians(90));
-        tileImage = ImageProcessing.verticalFlip(tileImage);
-      }
-
-      if (tile.isFlippedHorizontally()) {
-        tileImage = ImageProcessing.horizontalFlip(tileImage);
-      }
-
-      if (tile.isFlippedVertically()) {
-        tileImage = ImageProcessing.verticalFlip(tileImage);
-      }
-    }
-
-    return tileImage;
   }
 
   /**
@@ -176,8 +29,9 @@ public class HexagonalMapRenderer implements IMapRenderer {
    *          the map
    * @return the layer image
    */
-  private synchronized BufferedImage getLayerImage(final ITileLayer layer, final IMap map, boolean includeAnimationTiles) {
-    // if we have already retrived the image, use the one from the cache to
+  @Override
+  protected synchronized BufferedImage getLayerImage(final ITileLayer layer, final IMap map, boolean includeAnimationTiles) {
+    // if we have already retrieved the image, use the one from the cache to
     // draw the layer
     final String cacheKey = getCacheKey(map) + "_" + layer.getName();
     Optional<BufferedImage> opt = Resources.images().tryGet(cacheKey);
@@ -237,7 +91,8 @@ public class HexagonalMapRenderer implements IMapRenderer {
    * @param map
    * @param viewport
    */
-  private void renderTileLayerImage(final Graphics2D g, final ITileLayer layer, final IMap map, final Rectangle2D viewport) {
+  @Override
+  protected void renderTileLayerImage(final Graphics2D g, final ITileLayer layer, final IMap map, final Rectangle2D viewport) {
     // set alpha value of the tiles by the layers value
     final Composite oldComp = g.getComposite();
     final AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, layer.getOpacity());
@@ -271,23 +126,6 @@ public class HexagonalMapRenderer implements IMapRenderer {
       }
     }
 
-    g.setComposite(oldComp);
-  }
-
-  private void renderImageLayer(Graphics2D g, IImageLayer layer, Rectangle2D viewport) {
-    Spritesheet sprite = Resources.spritesheets().get(layer.getImage().getSource());
-    if (sprite == null) {
-      return;
-    }
-
-    final Composite oldComp = g.getComposite();
-    final AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, layer.getOpacity());
-    g.setComposite(ac);
-
-    final double viewportOffsetX = -viewport.getX() + layer.getOffset().x;
-    final double viewportOffsetY = -viewport.getY() + layer.getOffset().y;
-
-    ImageRenderer.render(g, sprite.getImage(), viewportOffsetX, viewportOffsetY);
     g.setComposite(oldComp);
   }
 }
---
+++ src/de/gurkenlabs/litiengine/environment/tilemap/MapRenderer.java
@@ -0,0 +1,185 @@
+package de.gurkenlabs.litiengine.environment.tilemap;
+
+import java.awt.AlphaComposite;
+import java.awt.Composite;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.util.Optional;
+
+import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.graphics.ImageRenderer;
+import de.gurkenlabs.litiengine.graphics.RenderType;
+import de.gurkenlabs.litiengine.graphics.Spritesheet;
+import de.gurkenlabs.litiengine.resources.Resources;
+import de.gurkenlabs.litiengine.util.ImageProcessing;
+
+public abstract class MapRenderer implements IMapRenderer {
+
+  @Override
+  public void render(final Graphics2D g, final IMap map, final Rectangle2D viewport, RenderType... renderTypes) {
+    for (final ILayer layer : map.getRenderLayers()) {
+      if (layer == null || !shouldBeRendered(layer, renderTypes)) {
+        continue;
+      }
+
+      if (layer instanceof ITileLayer) {
+        this.renderTileLayerImage(g, (ITileLayer) layer, map, viewport);
+      }
+
+      if (layer instanceof IImageLayer) {
+        renderImageLayer(g, (IImageLayer) layer, viewport);
+      }
+    }
+  }
+
+  @Override
+  public BufferedImage getImage(IMap map, RenderType... renderTypes) {
+    final String cacheKey = getCacheKey(map) + "_" + renderTypes;
+    Optional<BufferedImage> opt = Resources.images().tryGet(cacheKey);
+    if (opt.isPresent()) {
+      return opt.get();
+    }
+
+    final BufferedImage img = ImageProcessing.getCompatibleImage((int) map.getSizeInPixels().getWidth(), (int) map.getSizeInPixels().getHeight());
+    final Graphics2D g = img.createGraphics();
+
+    for (final ITileLayer layer : map.getTileLayers()) {
+      if (layer == null || !shouldBeRendered(layer, renderTypes)) {
+        continue;
+      }
+
+      ImageRenderer.render(g, this.getLayerImage(layer, map, true), layer.getOffset());
+    }
+
+    g.dispose();
+
+    Resources.images().add(cacheKey, img);
+    return img;
+  }
+
+  @Override
+  public BufferedImage getImage(IMap map) {
+    return this.getImage(map, RenderType.BACKGROUND, RenderType.GROUND, RenderType.SURFACE, RenderType.NORMAL, RenderType.OVERLAY);
+  }
+
+  @Override
+  public void render(final Graphics2D g, final IMap map, RenderType... renderTypes) {
+    this.render(g, map, 0, 0, renderTypes);
+  }
+
+  @Override
+  public void render(final Graphics2D g, final IMap map, final double offsetX, final double offsetY, RenderType... renderTypes) {
+    final BufferedImage mapImage = this.getImage(map, renderTypes);
+    ImageRenderer.render(g, mapImage, offsetX, offsetY);
+  }
+
+  protected abstract BufferedImage getLayerImage(final ITileLayer layer, final IMap map, boolean includeAnimationTiles);
+
+  protected abstract void renderTileLayerImage(final Graphics2D g, final ITileLayer layer, final IMap map, final Rectangle2D viewport);
+
+  protected static boolean shouldBeRendered(ILayer layer, RenderType[] renderTypes) {
+    if (renderTypes == null || renderTypes.length == 0) {
+      return isVisible(layer);
+    }
+
+    for (RenderType alloc : renderTypes) {
+      if (alloc == layer.getRenderType()) {
+        return isVisible(layer);
+      }
+    }
+
+    return false;
+  }
+
+  protected static boolean isVisible(ILayer layer) {
+    return layer.isVisible() && layer.getOpacity() > 0;
+  }
+
+  protected static void renderImageLayer(Graphics2D g, IImageLayer layer, Rectangle2D viewport) {
+    Spritesheet sprite = Resources.spritesheets().get(layer.getImage().getSource());
+    if (sprite == null) {
+      return;
+    }
+
+    final Composite oldComp = g.getComposite();
+    final AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, layer.getOpacity());
+    g.setComposite(ac);
+
+    final double viewportOffsetX = -viewport.getX() + layer.getOffset().x;
+    final double viewportOffsetY = -viewport.getY() + layer.getOffset().y;
+
+    ImageRenderer.render(g, sprite.getImage(), viewportOffsetX, viewportOffsetY);
+    g.setComposite(oldComp);
+  }
+
+  /**
+   * Gets the cache key.
+   *
+   * @param map
+   *          the map
+   * @return the cache key
+   */
+  protected static String getCacheKey(final IMap map) {
+    return "map_" + map.getName();
+  }
+
+  protected static Image getTileImage(final IMap map, final ITile tile) {
+    if (tile == null) {
+      return null;
+    }
+
+    final ITileset tileset = MapUtilities.findTileSet(map, tile);
+    if (tileset == null || tileset.getFirstGridId() > tile.getGridId()) {
+      return null;
+    }
+
+    Spritesheet sprite = tileset.getSpritesheet();
+    if (sprite == null) {
+      return null;
+    }
+
+    // get the grid id relative to the sprite sheet since we use a 0 based
+    // approach to calculate the position
+    int index = tile.getGridId() - tileset.getFirstGridId();
+
+    // support for animated tiles
+    final ITileAnimation animation = MapUtilities.getAnimation(map, index);
+    if (animation != null && !animation.getFrames().isEmpty()) {
+      final long playedMs = Game.time().sinceGameStart();
+
+      final int totalDuration = animation.getTotalDuration();
+      final long animationsPlayed = playedMs / totalDuration;
+
+      final long deltaTicks = playedMs - animationsPlayed * totalDuration;
+      int currentPlayTime = 0;
+      for (final ITileAnimationFrame frame : animation.getFrames()) {
+        currentPlayTime += frame.getDuration();
+        if (deltaTicks < currentPlayTime) {
+          // found the current animation frame
+          index = frame.getTileId();
+          break;
+        }
+      }
+    }
+
+    BufferedImage tileImage = sprite.getSprite(index, tileset.getMargin(), tileset.getSpacing());
+    if (tile.isFlipped()) {
+      if (tile.isFlippedDiagonally()) {
+        tileImage = ImageProcessing.rotate(tileImage, -Math.PI / 2);
+        tileImage = ImageProcessing.verticalFlip(tileImage);
+      }
+
+      if (tile.isFlippedHorizontally()) {
+        tileImage = ImageProcessing.horizontalFlip(tileImage);
+      }
+
+      if (tile.isFlippedVertically()) {
+        tileImage = ImageProcessing.verticalFlip(tileImage);
+      }
+    }
+
+    return tileImage;
+  }
+}
