471ce1302c33bbc0aaa1a3fdf7b58e2ee616d2ad
Sasha <sasha.hellstenius@gmail.com>
2019-02-12 19:39:08 +0100

    Fix #2 Add script for branchcoverage check

---
+++ branchtest/branchCoverage.py
@@ -0,0 +1,40 @@
+import random
+import argparse
+import sys
+import csv
+import numpy as np
+from numpy import loadtxt
+
+if __name__ == '__main__':
+
+    n = 10
+    outfile_name = 'result.txt'
+    filenames = ['test_01.csv', 'test_02.csv', 'test_03.csv', 'test_04.csv', 'test_05.csv',
+                 'test_06.csv', 'test_07.csv', 'test_08.csv', 'test_09.csv', 'test_10.csv']
+    
+    for i in range(n):
+        if i != 9:
+            continue
+        # Load all data from a specific funciton, each column represents a branch path
+        data = loadtxt(filenames[i], dtype=int, delimiter=',')
+        
+        # Sum over columns to see what branches are untested
+        data = np.sum(data, axis=0)
+        data[data == 0] = -1
+        data[data != -1] = 0
+        data = data*-1
+
+        nb = len(data)
+        coverage = round((nb - np.sum(data))/nb,2)
+        print(nb)
+
+        print (data)
+
+        with open(outfile_name, 'w') as outfile:
+            outfile.write("----------------------------------------\n")
+            outfile.write("ID: " + str(i) + " Coverage: " + str(coverage) +  "% Branches: " + str(nb) + "\n")
+            # outfile.write("BranchID  -  Covered\n")
+            for j in range(nb):
+                outfile.write(str(j+1) + " - "+ str(data[j]) + '\n')
+
+    sys.stdout.flush()
