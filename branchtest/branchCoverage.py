e1573caf3389e70615ca03755ff15ca6c415af3b
Gustaf Gunér <gustaf@guner.se>
2019-02-13 19:27:42 +0100

    Fix #5 Gradle always re-builds and re-runs the tests

--- branchtest/branchCoverage.py
+++ branchtest/branchCoverage.py
@@ -6,18 +6,18 @@ import numpy as np
 from numpy import loadtxt
 
 if __name__ == '__main__':
-
     n = 10
     outfile_name = 'result.txt'
     filenames = ['test_01.csv', 'test_02.csv', 'test_03.csv', 'test_04.csv', 'test_05.csv',
                  'test_06.csv', 'test_07.csv', 'test_08.csv', 'test_09.csv', 'test_10.csv']
-    
+
     for i in range(n):
-        if i != 9:
-            continue
         # Load all data from a specific funciton, each column represents a branch path
-        data = loadtxt(filenames[i], dtype=int, delimiter=',')
-        
+        try:
+            data = loadtxt(filenames[i], dtype=int, delimiter=',')
+        except IOError:
+            continue
+
         # Sum over columns to see what branches are untested
         data = np.sum(data, axis=0)
         data[data == 0] = -1
@@ -25,13 +25,13 @@ if __name__ == '__main__':
         data = data*-1
 
         nb = len(data)
-        coverage = round((nb - np.sum(data))/nb,2)
+        coverage = round((nb - np.sum(data))/nb, 2)
 
         with open(outfile_name, 'w') as outfile:
             outfile.write("----------------------------------------\n")
-            outfile.write("ID: " + str(i) + " Coverage: " + str(coverage) +  "% Branches: " + str(nb) + "\n")
+            outfile.write("ID: " + str(i) + " Coverage: " +
+                          str(coverage) + "% Branches: " + str(nb) + "\n")
             # outfile.write("BranchID  -  Covered\n")
             for j in range(nb):
-                outfile.write(str(j+1) + " - "+ str(data[j]) + '\n')
-
+                outfile.write(str(j+1) + " - " + str(data[j]) + '\n')
     sys.stdout.flush()
--- branchtest/testCoverage.sh
+++ branchtest/testCoverage.sh
@@ -4,13 +4,13 @@
 
 echo "-----------------Running test coverage------------------"
 
-# Delete all old csv files
+# Delete all old .csv files
 find *.csv -type f -delete
 
-# Run gradle build
-# TODO: only runs if code updates! This is a sligt problem
+# Run gradle build followed by gradle test
 cd ../ 
-gradle build
+gradle cleanBuild build
+gradle cleanTest test
 
 cd branchtest/
 # Run branch coverage script
