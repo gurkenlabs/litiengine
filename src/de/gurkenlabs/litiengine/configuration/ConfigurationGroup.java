b01e662bf933c9418be1bf5253e7a2ea4021c9c3
Vera Blomkvist Karlsson <verakar@kth.se>
2019-02-15 11:55:13 +0100

    #7 Add branch coverage checking code for function 1

--- src/de/gurkenlabs/litiengine/configuration/ConfigurationGroup.java
+++ src/de/gurkenlabs/litiengine/configuration/ConfigurationGroup.java
@@ -4,6 +4,9 @@ import java.lang.reflect.Field;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 import de.gurkenlabs.litiengine.util.ReflectionUtilities;
 
@@ -61,39 +64,99 @@ public abstract class ConfigurationGroup {
    *          the properties
    */
   protected void storeProperties(final Properties properties) {
+    int numberOfBranches = 27;
+    int branches[] = new int [numberOfBranches];
+    branches[0] = 1;
     try {
+      branches[1] = 1;
       for (final Field field : this.getClass().getDeclaredFields()) {
+        branches[2] = 1;
         if (!field.isAccessible()) {
+          branches[3] = 1;
           field.setAccessible(true);
+        } else {
+          branches[4] = 1;
         }
         
         final String propertyKey = this.getPrefix() + field.getName();
         if (field.getType().equals(boolean.class)) {
+          branches[5] = 1;
           properties.setProperty(propertyKey, Boolean.toString(field.getBoolean(this)));
         } else if (field.getType().equals(int.class)) {
+          branches[6] = 1;
           properties.setProperty(propertyKey, Integer.toString(field.getInt(this)));
         } else if (field.getType().equals(float.class)) {
+          branches[7] = 1;
           properties.setProperty(propertyKey, Float.toString(field.getFloat(this)));
         } else if (field.getType().equals(double.class)) {
+          branches[8] = 1;
           properties.setProperty(propertyKey, Double.toString(field.getDouble(this)));
         } else if (field.getType().equals(byte.class)) {
+          branches[9] = 1;
           properties.setProperty(propertyKey, Byte.toString(field.getByte(this)));
         } else if (field.getType().equals(short.class)) {
+          branches[10] = 1;
           properties.setProperty(propertyKey, Short.toString(field.getShort(this)));
         } else if (field.getType().equals(long.class)) {
+          branches[11] = 1;
           properties.setProperty(propertyKey, Long.toString(field.getLong(this)));
         } else if (field.getType().equals(String.class)) {
+          branches[12] = 1;
+          if(field.get(this) != null) {
+            branches[13] = 1;
+          } else {
+            branches[14] = 1;
+          }
           properties.setProperty(propertyKey, field.get(this) != null ? (String) field.get(this) : "");
         } else if (field.getType().equals(String[].class)) {
+          branches[15] = 1;
+          if(field.get(this) != null) {
+            branches[16] = 1;
+          } else {
+            branches[17] = 1;
+          }
           properties.setProperty(propertyKey, field.get(this) != null ? String.join(",", (String[]) field.get(this)) : "");
         } else if (field.getType().isEnum()) {
+          branches[18] = 1;
           Object val = field.get(this);
+          if(val == null && field.getType().getEnumConstants().length > 0) {
+            branches[19] = 1;
+          } else {
+            branches[20] = 1;
+          }
           final String value = val == null && field.getType().getEnumConstants().length > 0 ? field.getType().getEnumConstants()[0].toString() : "";
+          if(val != null) {
+            branches[21] = 1;
+          }
+          else {
+            branches[22] = 1;
+          }
           properties.setProperty(propertyKey, val != null ? val.toString() : value);
+        } else {
+          branches[23] = 1;
         }
       }
+      branches[24] = 1;
     } catch (final IllegalArgumentException | IllegalAccessException e) {
+      branches[25] = 1;
       log.log(Level.SEVERE, e.getMessage(), e);
+    }
+    branches[26] = 1;
+    try {
+      FileWriter fw = new FileWriter("branchtest/test_01.csv", true);
+      BufferedWriter bw = new BufferedWriter(fw);
+      PrintWriter pw = new PrintWriter(bw);
+      pw.print(branches[0]);
+      for (int i = 1; i < numberOfBranches; i++) {
+        pw.print(",");
+        pw.print(branches[i]);
+      }
+      pw.println();
+      pw.flush();
+      bw.close();
+      fw.close();
+    } catch (Exception e) {
+      System.exit(1);
     }
   }
 
