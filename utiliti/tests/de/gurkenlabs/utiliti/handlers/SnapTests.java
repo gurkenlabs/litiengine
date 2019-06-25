cc2892a975e3869030b5b7c79d58f7918bc0382c
Calvin M.T <calvin.massonnet@gmail.com>
2019-06-25 14:19:00 +0200

    Add tests for snapping divisions

--- utiliti/tests/de/gurkenlabs/utiliti/handlers/SnapTests.java
+++ utiliti/tests/de/gurkenlabs/utiliti/handlers/SnapTests.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import de.gurkenlabs.litiengine.util.MathUtilities;
+import de.gurkenlabs.utiliti.components.Editor;
+
 public class SnapTests {
   @Test
   public void testPixelSnapping() {
@@ -28,5 +31,56 @@ public class SnapTests {
 
     assertEquals(0, Snap.y(7, 16, true, true));
     assertEquals(16, Snap.y(9.91, 16, true, true));
+  }
+  
+  @Test
+  public void testGridSnappingDivision() {
+    float aThird = (float) MathUtilities.round(16.0/3.0, 2);
+    float twoThirds = (float) MathUtilities.round((16.0/3.0)*2.0, 2);
+
+    Editor.preferences().setSnapDivision(3);
+
+    assertEquals(0, Snap.x(1.1, 16, true, false));
+    assertEquals(aThird, Snap.x(6, 16, true, false));
+    assertEquals(twoThirds, Snap.x(11.1, 16, true, false));
+    assertEquals(16, Snap.x(14, 16, true, false));
+
+    assertEquals(0, Snap.y(2, 16, true, false));
+    assertEquals(aThird, Snap.y(7.99, 16, true, false));
+    assertEquals(twoThirds, Snap.y(9.91, 16, true, false));
+    assertEquals(16, Snap.y(15, 16, true, false));
+
+    // enabling pixel snapping while grid snapping is active should not make any difference
+    assertEquals(0, Snap.x(1.1, 16, true, true));
+    assertEquals(aThird, Snap.x(6, 16, true, true));
+    assertEquals(twoThirds, Snap.x(11.1, 16, true, true));
+    assertEquals(16, Snap.x(14, 16, true, true));
+
+    assertEquals(0, Snap.y(2, 16, true, true));
+    assertEquals(aThird, Snap.y(7.99, 16, true, true));
+    assertEquals(twoThirds, Snap.y(9.91, 16, true, true));
+    assertEquals(16, Snap.y(14, 16, true, true));
+
+    Editor.preferences().setSnapDivision(2);
+
+    assertEquals(0, Snap.x(1.1, 16, true, false));
+    assertEquals(16/2, Snap.x(11.99, 16, true, false));
+    assertEquals(16, Snap.x(14, 16, true, false));
+
+    assertEquals(0, Snap.y(3.99, 16, true, false));
+    assertEquals(16/2, Snap.y(9.91, 16, true, false));
+    assertEquals(16, Snap.y(12, 16, true, false));
+
+    // enabling pixel snapping while grid snapping is active should not make any difference
+    assertEquals(0, Snap.x(1.1, 16, true, true));
+    assertEquals(16/2, Snap.x(11.99, 16, true, true));
+    assertEquals(16, Snap.x(14, 16, true, true));
+
+    assertEquals(0, Snap.y(3.99, 16, true, true));
+    assertEquals(16/2, Snap.y(9.91, 16, true, true));
+    assertEquals(16, Snap.y(12, 16, true, true));
+
+    // reset to no divisions
+    Editor.preferences().setSnapDivision(1);
   }
 }
