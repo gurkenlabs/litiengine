f2e10c95b9bc6dbacb9a2731e9c084a66d089164
Steffen Wilke <mail@steffen-wilke.de>
2019-07-02 01:22:45 +0200

    Add initialization test for the MobileEntity class.

---
+++ tests/de/gurkenlabs/litiengine/entities/MobileEntityTests.java
@@ -0,0 +1,25 @@
+package de.gurkenlabs.litiengine.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+import de.gurkenlabs.litiengine.annotation.MovementInfo;
+
+public class MobileEntityTests {
+  @Test
+  public void testInitializationByAnnotation() {
+    IMobileEntity entity = new TestMobileEntity();
+
+    assertEquals(111, entity.getVelocity().getCurrentValue().intValue());
+    assertEquals(222, entity.getAcceleration());
+    assertEquals(333, entity.getDeceleration());
+    assertFalse(entity.turnOnMove());
+  }
+
+  @MovementInfo(velocity = 111, acceleration = 222, deceleration = 333, turnOnMove = false)
+  private class TestMobileEntity extends MobileEntity {
+
+  }
+}
