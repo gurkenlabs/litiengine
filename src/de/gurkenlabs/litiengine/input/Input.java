31e2470a93c8718aab8a4d4704d26a154f916e71
Steffen Wilke <mail@steffen-wilke.de>
2020-02-23 14:40:30 +0100

    Add some javadoc.

--- src/de/gurkenlabs/litiengine/input/Input.java
+++ src/de/gurkenlabs/litiengine/input/Input.java
@@ -26,6 +26,24 @@ public final class Input {
     throw new UnsupportedOperationException();
   }
 
+  /**
+   * Gets the manager for all gamepad input devices.
+   * 
+   * <p>
+   * The manager provides easy access to the default controller as well as access by gamepad index for mulitplayer games.
+   * Gamepads don't need to be added explicitly, the manager supports hot-plugging at runtime and will auto-detect any
+   * added/removed gamepads.
+   * </p>
+   * 
+   * <p>
+   * <b>This returns null if <code>Game.config().input().isGamepadSupport()</code> is set to false.</b>
+   * </p>
+   * 
+   * @return The gamepad manager.
+   * 
+   * @see GamepadManager#current()
+   * @see GamepadManager#get(int)
+   */
   public static GamepadManager gamepads() {
     if (!Game.config().input().isGamepadSupport()) {
       log.log(Level.SEVERE, "Cannot access gamepads because gamepad support is disabled in the configuration.");
@@ -34,10 +52,20 @@ public final class Input {
     return gamePadManager;
   }
 
+  /**
+   * Gets the keyboard input device.
+   * 
+   * @return The keyboard input device.
+   */
   public static IKeyboard keyboard() {
     return keyboard;
   }
 
+  /**
+   * Gets the mouse input device.
+   * 
+   * @return The mouse input device.
+   */
   public static IMouse mouse() {
     return mouse;
   }
@@ -66,13 +94,13 @@ public final class Input {
       try {
         keyboard = new Keyboard();
         Mouse m = new Mouse();
-        
+
         if (!Game.isInNoGUIMode()) {
           Game.window().getRenderComponent().addMouseListener(m);
           Game.window().getRenderComponent().addMouseMotionListener(m);
           Game.window().getRenderComponent().addMouseWheelListener(m);
         }
-        
+
         mouse = m;
         if (Game.config().input().isGamepadSupport()) {
           gamePadManager = new GamepadManager();
