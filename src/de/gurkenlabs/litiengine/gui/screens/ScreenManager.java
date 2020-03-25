7c1f3d2e29a8e90b7d66ae458b7d263ed0bc4f2c
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2020-03-25 10:50:50 -0400

    Use fully qualified imports

--- src/de/gurkenlabs/litiengine/gui/screens/ScreenManager.java
+++ src/de/gurkenlabs/litiengine/gui/screens/ScreenManager.java
@@ -1,6 +1,9 @@
 package de.gurkenlabs.litiengine.gui.screens;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
