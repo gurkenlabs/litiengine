d8c9e69690f32d6380bfcc70ac81cbd059ea4e2e
nightm4re94 <matthias@gurkenlabs.de>
2020-04-19 22:10:00 +0200

    Make dynamic Location updates for EntityEmitters optional.

--- src/de/gurkenlabs/litiengine/graphics/emitters/EntityEmitter.java
+++ src/de/gurkenlabs/litiengine/graphics/emitters/EntityEmitter.java
@@ -12,6 +12,7 @@ import de.gurkenlabs.litiengine.entities.IEntity;
 public abstract class EntityEmitter extends Emitter {
 
   private final IEntity entity;
+  private boolean dynamicLocation;
 
   /**
    * Instantiates a new entity emitter.
@@ -20,13 +21,31 @@ public abstract class EntityEmitter extends Emitter {
    *          the entity
    */
   public EntityEmitter(final IEntity entity) {
+    this(entity, false);
+  }
+
+  /**
+   * Instantiates a new entity emitter.
+   * 
+   *
+   * @param entity
+   *          the entity
+   * @param dynamicLocation
+   *          if true, move the Emitter along with its Entity once it moves. If false, always keep the original Location of the Emitter
+   */
+  public EntityEmitter(final IEntity entity, boolean dynamicLocation) {
     super(entity.getX(), entity.getY());
     this.entity = entity;
     this.setSize(this.getEntity().getWidth(), this.getEntity().getHeight());
+    this.dynamicLocation = dynamicLocation;
   }
 
   public IEntity getEntity() {
     return this.entity;
+  }
+
+  public boolean hasDynamicLocation() {
+    return this.dynamicLocation;
   }
 
   @Override
@@ -34,7 +53,7 @@ public abstract class EntityEmitter extends Emitter {
     if (this.getEntity() == null) {
       return null;
     }
-    return this.getEntity().getLocation();
+    return this.hasDynamicLocation() ? this.getEntity().getLocation() : super.getLocation();
   }
 
 }
