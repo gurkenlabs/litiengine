0768f55f160041b980f0b486855a8b87ac4c5843
Jan De Blander <jandeblander@hotmail.com>
2020-11-25 16:50:15 +0100

    Corrected imports of unit tests

--- tests/de/gurkenlabs/litiengine/attributes/AttributeModifierTest.java
+++ tests/de/gurkenlabs/litiengine/attributes/AttributeModifierTest.java
@@ -2,7 +2,8 @@ package de.gurkenlabs.litiengine.attributes;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AttributeModifierTest {
 
--- tests/de/gurkenlabs/litiengine/attributes/RangeAttributeTest.java
+++ tests/de/gurkenlabs/litiengine/attributes/RangeAttributeTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RangeAttributeTest {
 
