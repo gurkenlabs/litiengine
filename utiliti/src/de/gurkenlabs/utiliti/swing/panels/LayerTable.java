7f7685c06582e9f25f47123cee25da78f376e90d
nightm4re94 <matthias@gurkenlabs.de>
2021-02-07 20:47:41 +0100

    Prevent another NullPointer in the layer table.

--- utiliti/src/de/gurkenlabs/utiliti/swing/panels/LayerTable.java
+++ utiliti/src/de/gurkenlabs/utiliti/swing/panels/LayerTable.java
@@ -98,7 +98,11 @@ public class LayerTable extends JTable {
       @Override
       public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-        if (column == 0 && Game.world().environment().getMap() != null && Game.world().environment().getMap().getMapObjectLayers().get(row) != null) {
+        if (Game.world().environment() == null || Game.world().environment().getMap() == null
+            || Game.world().environment().getMap().getMapObjectLayers().get(row) == null) {
+          return c;
+        }
+        if (column == 0) {
           c.setBackground(Game.world().environment().getMap().getMapObjectLayers().get(row).getColor());
         }
         return c;
