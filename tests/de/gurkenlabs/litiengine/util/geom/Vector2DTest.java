a5b85c4aa625b26749a5cab82bf0d0982bc1b6bd
nwessman <nwessman@kth.se>
2021-02-26 11:25:40 +0100

    Add Vector2D add test
    This adds a test for Vector2D class that asserts that the add function works for all three types of initiations of an object.

---
+++ tests/de/gurkenlabs/litiengine/util/geom/Vector2DTest.java
@@ -0,0 +1,37 @@
+package de.gurkenlabs.litiengine.util.geom;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.awt.geom.Point2D;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+public class Vector2DTest {
+
+    /**
+     * Test add-function in Vector2D class
+     */
+    @Test
+    public void testVector2D() {
+        double xValue = 10.0;
+        double yValue = 5.0;
+        Point2D point1 = new Point2D.Double(0, 0);;
+        Point2D point2 = new Point2D.Double(xValue, yValue);;
+
+        Vector2D vectorStandard = new Vector2D();
+        Vector2D vectorDouble = new Vector2D(xValue, yValue);
+        Vector2D vectorPoint2D = new Vector2D(point1, point2);
+
+        Vector2D vectorResult1 = vectorStandard.add(vectorDouble);
+        Vector2D vectorResult2 = vectorStandard.add(vectorPoint2D);
+
+        assertEquals(vectorResult1.dX, xValue);
+        assertEquals(vectorResult1.dY, yValue);
+        assertEquals(vectorResult2.dX, xValue);
+        assertEquals(vectorResult2.dY, yValue);
+        assertEquals(vectorResult1.dX, vectorResult1.getX());
+        assertEquals(vectorResult1.dY, vectorResult1.getY());
+
+    }
+}
