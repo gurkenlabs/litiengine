7e53123f364bb22931707ff76b56a378c1f7b284
nwessman <nwessman@kth.se>
2021-03-08 12:24:24 +0100

    Add functionality to create json with frames object
    This adds a function that creates the aseprite json as a string representation. it currently only works for the frames object and does not include the meta-data object.

--- src/de/gurkenlabs/litiengine/graphics/animation/AsepriteHandler.java
+++ src/de/gurkenlabs/litiengine/graphics/animation/AsepriteHandler.java
@@ -5,14 +5,84 @@ import java.awt.Image;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.SerializedName;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 import de.gurkenlabs.litiengine.graphics.animation.Animation;
 import de.gurkenlabs.litiengine.graphics.animation.KeyFrame;
 import de.gurkenlabs.litiengine.graphics.Spritesheet;
 
 public class AsepriteHandler {
+
+    
+    public void exportAnimation(Animation animation){
+
+        String json = createJson(animation);
+        System.out.println("JSON: " + json);
+    }
+
+    private String createJson(Animation animation){
+        Spritesheet spritesheet = animation.getSpritesheet();
+        List<KeyFrame> keyframes = animation.getKeyframes();
+        Frames[] frames = new Frames[keyframes.size()];
+
+        if(frames.length != spritesheet.getTotalNumberOfSprites()){
+            //ERROR
+            System.out.println("ERROR"); 
+        }
+
+        int numCol = spritesheet.getColumns();
+        int numRows = spritesheet.getRows();
+        int frameWidth = spritesheet.getSpriteWidth();
+        int frameHeight = spritesheet.getSpriteHeight();
+
+        for(int i = 0; i < numRows; i++){
+            for(int j = 0; j < numCol; j++){
+                final int row = i;
+                final int col = j;
+                Map<String, Integer> frame = new HashMap<>(){{
+                    put("x", (0 + col*frameWidth) );
+                    put("y", (0 + row*frameHeight) );
+                    put("w", frameWidth);
+                    put("h", frameHeight);
+                }};
+                Map<String, Integer> spriteSourceSize = new HashMap<>(){{
+                    put("x", 0);
+                    put("y", 0);
+                    put("w", frameWidth);
+                    put("h", frameHeight);
+                }};
+                Map<String, Integer> sourceSize = new HashMap<>(){{
+                    put("w", frameWidth);
+                    put("h", frameHeight);
+                }};
+                int duration = keyframes.get(i+j).getDuration();
+                frames[i+j] = new Frames("frame " + i*j, 
+                                        frame, 
+                                        false, 
+                                        false, 
+                                        spriteSourceSize, 
+                                        sourceSize, 
+                                        duration);
+            }
+        }
+
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("{ \"frames\": {\n");
+        for(int i = 0; i < frames.length; i++){
+            String json = gson.toJson(frames[i]);
+            sb.append(" \"" + frames[i].name + "\": ").append(json).append(",\n");
+        }
+        sb.append(" },\n");
+        //System.out.println("JSON:\n" + sb.toString());
+
+        return sb.toString();
+    }
 
     private class Frames {
         transient String name; 
