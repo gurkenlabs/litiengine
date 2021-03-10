3bc578447f4f5f1205ef390dbce5a8b5a202d2ce
jluech <janik@luechinger.ch>
2021-03-10 16:28:25 +0100

    refactor: match file name with tests content

--- tests/de/gurkenlabs/litiengine/GameTimeTests.java
+++ tests/de/gurkenlabs/litiengine/GameTimeTests.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class GameLoopTest {
+public class GameTimeTests {
 
   @Test
   public void testTimeCalculation100() {
