bbbbe937108fe09086d320ef0f1051737eeb7e56
Niels Kübler <niels.kuebler@uzh.ch>
2021-03-14 17:25:23 +0100

    Cleaned up PropTests and CustomPropertyAdapterTests

--- tests/de/gurkenlabs/litiengine/entities/PropTests.java
+++ tests/de/gurkenlabs/litiengine/entities/PropTests.java
@@ -9,36 +9,45 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PropTests {
 
     @Test
-    public void testPropStateIntact(){
+    public void testGetStateIntact(){
+        // arrange
         Prop prop = new Prop(0, 0, null);
         prop.setIndestructible(true);
 
+        // act
         PropState propState = prop.getState();
 
+        // assert
         assertEquals(PropState.INTACT, propState);
         assertEquals(false, prop.isDead());
     }
 
     @Test
-    public void testPropStateDamaged(){
+    public void testGetStateDamaged(){
+        // arrange
         Prop prop = new Prop(0, 0, null);
         prop.setIndestructible(false);
         prop.getHitPoints().modifyBaseValue(new AttributeModifier<>(Modification.SUBTRACT, 60));
 
+        // act
         PropState propState = prop.getState();
 
+        // assert
         assertEquals(PropState.DAMAGED, propState);
         assertEquals(false, prop.isDead());
     }
 
     @Test
-    public void testPropStateDestroyed(){
+    public void testGetStateDestroyed(){
+        // arrange
         Prop prop = new Prop(0, 0, null);
         prop.setIndestructible(false);
         prop.getHitPoints().modifyBaseValue(new AttributeModifier<>(Modification.SUBTRACT, 150));
 
+        // act
         PropState propState = prop.getState();
 
+        // assert
         assertEquals(PropState.DESTROYED, propState);
         assertEquals(true, prop.isDead());
 
--- tests/de/gurkenlabs/litiengine/environment/tilemap/xml/CustomPropertyAdapterTests.java
+++ tests/de/gurkenlabs/litiengine/environment/tilemap/xml/CustomPropertyAdapterTests.java
@@ -7,18 +7,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CustomPropertyAdapterTests {
 
     @Test
-    public void testPropertyComparison(){
+    public void testCompareTo(){
+        // arrange
         CustomPropertyAdapter.Property propertyWithNameA = new CustomPropertyAdapter.Property("PropertyA", "float");
         CustomPropertyAdapter.Property propertyWithNameB = new CustomPropertyAdapter.Property("PropertyB", "float");
-        CustomPropertyAdapter.Property propertyNullNameA = new CustomPropertyAdapter.Property(null, "");
-        CustomPropertyAdapter.Property propertyNullNameB = new CustomPropertyAdapter.Property(null, "");
 
-        assertEquals(1, propertyWithNameA.compareTo(null));
-        assertEquals(0, propertyNullNameA.compareTo(propertyNullNameB));
-        assertEquals(-1, propertyNullNameA.compareTo(propertyWithNameA));
+        // act
+        int result1 = propertyWithNameA.compareTo(propertyWithNameB);
+        int result2 = propertyWithNameB.compareTo(propertyWithNameA);
+        int result3 = propertyWithNameA.compareTo(propertyWithNameA);
 
-        assertEquals(-1, propertyWithNameA.compareTo(propertyWithNameB));
-        assertEquals(1, propertyWithNameB.compareTo(propertyWithNameA));
-        assertEquals(0, propertyWithNameA.compareTo(propertyWithNameA));
+        // assert
+        assertEquals(-1, result1);
+        assertEquals(1, result2);
+        assertEquals(0, result3);
+    }
+
+    @Test
+    public void testCompareToNull(){
+        // arrange
+        CustomPropertyAdapter.Property propertyWithName = new CustomPropertyAdapter.Property("PropertyA", "float");
+        CustomPropertyAdapter.Property propertyNullNameNoType = new CustomPropertyAdapter.Property(null, "");
+
+        // act
+        int result1 = propertyWithName.compareTo(null);
+        int result2 = propertyNullNameNoType.compareTo(propertyWithName);
+        int result3 = propertyNullNameNoType.compareTo(propertyNullNameNoType);
+
+        // assert
+        assertEquals(1, result1);
+        assertEquals(-1, result2);
+        assertEquals(0, result3);
     }
 }
