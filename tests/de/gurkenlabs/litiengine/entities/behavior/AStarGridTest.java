f9cf929427bdb95e0aac6bc16109f837b855d900
ddreimane <dreimane.dace@gmail.com>
2021-04-19 01:07:49 +0200

    Remove unnecessary imports

--- tests/de/gurkenlabs/litiengine/entities/behavior/AStarGridTest.java
+++ tests/de/gurkenlabs/litiengine/entities/behavior/AStarGridTest.java
@@ -1,19 +1,15 @@
 package de.gurkenlabs.litiengine.entities.behavior;
 
-import de.gurkenlabs.litiengine.Game;
-import de.gurkenlabs.litiengine.abilities.Ability;
-import de.gurkenlabs.litiengine.graphics.IRenderable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.awt.geom.Rectangle2D;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.when;
+
 
 public class AStarGridTest {
     private Rectangle2D rectangle;
