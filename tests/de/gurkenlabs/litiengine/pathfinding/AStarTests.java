1c2844b46bc58ef78d8faa2a1319d53dcdc6cb9a
Niels Kübler <niels.kuebler@uzh.ch>
2021-04-24 12:23:32 +0200

    Refactored AStarTests::testCostCalculationNode to reduce code duplication and improve understandability.

--- tests/de/gurkenlabs/litiengine/pathfinding/AStarTests.java
+++ tests/de/gurkenlabs/litiengine/pathfinding/AStarTests.java
@@ -12,6 +12,7 @@ import java.awt.Rectangle;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -25,6 +26,9 @@ import de.gurkenlabs.litiengine.entities.behavior.AStarNode;
 import de.gurkenlabs.litiengine.environment.Environment;
 import de.gurkenlabs.litiengine.environment.tilemap.IMap;
 import de.gurkenlabs.litiengine.environment.tilemap.MapOrientations;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class AStarTests {
   @BeforeAll
@@ -51,38 +55,58 @@ public class AStarTests {
     Game.world().loadEnvironment(new Environment(map));
   }
 
-  @Test
-  public void testCostCalculationNode() {
-    AStarNode node = new AStarNode(false, new Rectangle(50, 50, 10, 10), 5, 5);
-    AStarNode nodeDown = new AStarNode(false, new Rectangle(50, 100, 10, 10), 5, 10);
-    AStarNode nodeUp = new AStarNode(false, new Rectangle(50, 40, 10, 10), 5, 4);
-    AStarNode nodeLeft = new AStarNode(false, new Rectangle(30, 50, 10, 10), 3, 5);
-    AStarNode nodeRight = new AStarNode(false, new Rectangle(60, 50, 10, 10), 6, 5);
+  @ParameterizedTest(name = "testGetCostsSimpleDirections: {0}, expectedCost={2}")
+  @MethodSource("getCostsSimpleDirectionsArguments")
+  public void testGetCostsSimpleDirections(String name, AStarNode remoteNode, double expectedCost){
+    // arrange
+    AStarNode originNode = new AStarNode(false, new Rectangle(50, 50, 10, 10), 5, 5);
 
-    AStarNode nodeDownLeft = new AStarNode(false, new Rectangle(40, 60, 10, 10), 4, 6);
-    AStarNode nodeDownRight = new AStarNode(false, new Rectangle(60, 60, 10, 10), 6, 6);
-    AStarNode nodeUpLeft = new AStarNode(false, new Rectangle(40, 40, 10, 10), 4, 4);
-    AStarNode nodeUpRight = new AStarNode(false, new Rectangle(60, 40, 10, 10), 6, 4);
+    // act
+    double actualCost = originNode.getCosts(remoteNode);
 
-    double costDown = node.getCosts(nodeDown);
-    double costUp = node.getCosts(nodeUp);
-    double costLeft = node.getCosts(nodeLeft);
-    double costRight = node.getCosts(nodeRight);
+    // assert
+    assertEquals(expectedCost, actualCost);
+  }
 
-    double costDownLeft = node.getCosts(nodeDownLeft);
-    double costDownRight = node.getCosts(nodeDownRight);
-    double costUpLeft = node.getCosts(nodeUpLeft);
-    double costUpRight = node.getCosts(nodeUpRight);
+  /**
+   * This method is used to fill in the arguments of the parametrized test {@link #testGetCostsSimpleDirections(String, AStarNode, double)}
+   * @return Test arguments
+   */
+  @SuppressWarnings("unused")
+  private static Stream<Arguments> getCostsSimpleDirectionsArguments(){
+    return Stream.of(
+            Arguments.of("down", new AStarNode(false, new Rectangle(50, 100, 10, 10), 5, 10), 5.0),
+            Arguments.of("up", new AStarNode(false, new Rectangle(50, 40, 10, 10), 5, 4), 1.0),
+            Arguments.of("left", new AStarNode(false, new Rectangle(30, 50, 10, 10), 3, 5), 2.0),
+            Arguments.of("right", new AStarNode(false, new Rectangle(60, 50, 10, 10), 6, 5), 1.0)
+    );
+  }
 
-    assertEquals(5.0, costDown);
-    assertEquals(1.0, costUp);
-    assertEquals(2.0, costLeft);
-    assertEquals(1.0, costRight);
+  @ParameterizedTest(name = "testGetCostsAdvancedDirections: {0}")
+  @MethodSource("getCostsAdvancedDirectionsArguments")
+  public void testGetCostsAdvancedDirections(String name, AStarNode remoteNode){
+    // arrange
+    AStarNode originNode = new AStarNode(false, new Rectangle(50, 50, 10, 10), 5, 5);
 
-    assertTrue(costDownLeft > 1);
-    assertTrue(costDownRight > 1);
-    assertTrue(costUpLeft > 1);
-    assertTrue(costUpRight > 1);
+    // act
+    double actualCost = originNode.getCosts(remoteNode);
+
+    // assert
+    assertTrue(actualCost > 1);
+  }
+
+  /**
+   * This method is used to fill in the arguments of the parametrized test {@link #testGetCostsAdvancedDirections(String, AStarNode)}
+   * @return Test arguments
+   */
+  @SuppressWarnings("unused")
+  private static Stream<Arguments> getCostsAdvancedDirectionsArguments(){
+    return Stream.of(
+            Arguments.of("downLeft", new AStarNode(false, new Rectangle(40, 60, 10, 10), 4, 6)),
+            Arguments.of("downRight", new AStarNode(false, new Rectangle(60, 60, 10, 10), 6, 6)),
+            Arguments.of("upLeft", new AStarNode(false, new Rectangle(40, 40, 10, 10), 4, 4)),
+            Arguments.of("upRight", new AStarNode(false, new Rectangle(60, 40, 10, 10), 6, 4))
+    );
   }
 
   @Test
