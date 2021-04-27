989614a477d2cf8ad2e9b01e743d2f44ef192fd7
jluech <janik@luechinger.ch>
2021-04-28 00:48:12 +0200

    test: kill mutants and raise line coverage to 100% for MapArea

---
+++ tests/de/gurkenlabs/litiengine/entities/MapAreaTests.java
@@ -0,0 +1,51 @@
+package de.gurkenlabs.litiengine.entities;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.geom.Point2D;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class MapAreaTests {
+    @Test
+    public void testConstructorDefault() {
+        // act
+        MapArea mapArea = new MapArea();
+
+        // assert
+        assertNotNull(mapArea);
+        assertEquals(0, mapArea.getMapId()); // default
+        assertNull(mapArea.getName());
+    }
+
+    @Test
+    public void testConstructorWithArgs() {
+        // act
+        MapArea mapArea = new MapArea(1, 2, 3, 4);
+
+        // assert
+        assertEquals(0, mapArea.getMapId());
+        assertNull(mapArea.getName());
+        assertEquals(1, mapArea.getX());
+        assertEquals(2, mapArea.getY());
+        assertEquals(3, mapArea.getWidth());
+        assertEquals(4, mapArea.getHeight());
+    }
+
+    @Test
+    public void testConstructorWithId() {
+        // act
+        MapArea mapArea = new MapArea(42, "cool name", 9, 8, 7, 6);
+
+        // assert
+        assertEquals(42, mapArea.getMapId());
+        assertEquals("cool name", mapArea.getName());
+        assertEquals(9, mapArea.getX());
+        assertEquals(8, mapArea.getY());
+        assertEquals(7, mapArea.getWidth());
+        assertEquals(6, mapArea.getHeight());
+        assertEquals(new Point2D.Double(9, 8), mapArea.getLocation());
+    }
+}
