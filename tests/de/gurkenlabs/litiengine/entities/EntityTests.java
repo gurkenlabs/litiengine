1e0aaa9891936ce12f5228f44edeb58823776eb2
jluech <janik@luechinger.ch>
2021-04-28 17:53:50 +0200

    test: add first batch of new Entity tests

--- tests/de/gurkenlabs/litiengine/entities/EntityTests.java
+++ tests/de/gurkenlabs/litiengine/entities/EntityTests.java
@@ -1,19 +1,197 @@
 package de.gurkenlabs.litiengine.entities;
 
+import de.gurkenlabs.litiengine.entities.behavior.IBehaviorController;
+import de.gurkenlabs.litiengine.graphics.RenderType;
+import de.gurkenlabs.litiengine.graphics.animation.IEntityAnimationController;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.awt.geom.Point2D;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 public class EntityTests {
+
+    @Test
+    public void testOnMessageDelegates() {
+        // arrange
+        TestEntity entitySpy = spy(new TestEntity());
+        EntityMessageListener listenerMock = mock(EntityMessageListener.class);
+
+        // act
+        entitySpy.onMessage(listenerMock);
+
+        // assert
+        verify(entitySpy, times(1)).onMessage(Entity.ANY_MESSAGE, listenerMock);
+    }
+
+    @Test
+    public void testGetAngle() {
+        // arrange
+        TestEntity entity = new TestEntity();
+
+        // act
+        double angle = entity.getAngle();
+
+        // assert
+        assertEquals(0, angle); // default
+    }
+
+    @Test
+    public void testSetAngle() {
+        // arrange
+        TestEntity entity = new TestEntity();
+        assertEquals(0, entity.getAngle()); // default
+
+        // act
+        entity.setAngle(5);
+
+        // assert
+        assertEquals(5, entity.getAngle());
+    }
+
+    @Test
+    public void testBehavior() {
+        // arrange
+        TestEntity entitySpy = spy(new TestEntity());
+
+        // act
+        entitySpy.behavior();
+
+        // assert
+        verify(entitySpy, times(1)).getController(IBehaviorController.class);
+    }
+
+    @Test
+    public void testGetCenter() {
+        // arrange
+        TestEntity entitySpy = spy(new TestEntity());
+        when(entitySpy.getX()).thenReturn(5d);
+        when(entitySpy.getWidth()).thenReturn(10d); // 5 + 10*0.5 = 10
+        when(entitySpy.getY()).thenReturn(15d);
+        when(entitySpy.getHeight()).thenReturn(20d); // 15 + 20*0.5 = 25
+
+        // act
+        Point2D center = entitySpy.getCenter();
+
+        // assert
+        assertEquals(new Point2D.Double(10d, 25d), center);
+    }
+
+    @Test
+    public void testGetRenderType() {
+        // arrange
+        TestEntity entity = new TestEntity();
+
+        // act
+        RenderType type = entity.getRenderType();
+
+        // assert
+        assertEquals(RenderType.NORMAL, type); // default
+    }
+
+    @Test
+    public void testSetRenderType() {
+        // arrange
+        TestEntity entity = new TestEntity();
+        assertEquals(RenderType.NORMAL, entity.getRenderType()); // default
+
+        // act
+        entity.setRenderType(RenderType.SURFACE);
+
+        // assert
+        assertEquals(RenderType.SURFACE, entity.getRenderType());
+    }
+
+    @Test
+    public void testIsVisibleNull() {
+        // arrange
+        TestEntity entitySpy = spy(new TestEntity());
+        when(entitySpy.animations()).thenReturn(null);
+
+        // act, assert
+        assertFalse(entitySpy.isVisible());
+    }
+
+    @Test
+    public void testIsVisibleNotEnabled() {
+        // arrange
+        TestEntity entitySpy = spy(new TestEntity());
+        IEntityAnimationController animationControllerMock = mock(IEntityAnimationController.class);
+        when(animationControllerMock.isEnabled()).thenReturn(false);
+        when(entitySpy.animations()).thenReturn(animationControllerMock);
+
+        // act, assert
+        assertFalse(entitySpy.isVisible());
+    }
+
+    @Test
+    public void testIsVisibleEnabled() {
+        // arrange
+        TestEntity entitySpy = spy(new TestEntity());
+        IEntityAnimationController animationControllerMock = mock(IEntityAnimationController.class);
+        when(animationControllerMock.isEnabled()).thenReturn(true);
+        when(entitySpy.animations()).thenReturn(animationControllerMock);
+
+        // act, assert
+        assertTrue(entitySpy.isVisible());
+    }
+
+    @Test
+    public void testSetVisibleAnimationsNull() {
+        // arrange
+        TestEntity entitySpy = spy(new TestEntity());
+        when(entitySpy.animations()).thenReturn(null);
+
+        // act
+        entitySpy.setVisible(true);
+
+        // assert
+        verify(entitySpy, times(1)).setVisible(true); // obvious, but would trigger noMoreInteractions
+        verify(entitySpy, times(1)).animations();
+        verifyNoMoreInteractions(entitySpy);
+    }
+
+    @Test
+    public void testSetVisibleDelegates() {
+        // arrange
+        TestEntity entitySpy = spy(new TestEntity());
+        IEntityAnimationController animationControllerMock = mock(IEntityAnimationController.class);
+        when(entitySpy.animations()).thenReturn(animationControllerMock);
+
+        // act
+        entitySpy.setVisible(true);
+
+        // assert
+        verify(animationControllerMock, times(1)).setEnabled(true);
+    }
+
+    @Test
+    public void testSendMessage() {
+        // arrange
+        TestEntity entitySpy = spy(new TestEntity());
+        Creature sender = mock(Creature.class); // arbitrary object
+
+        // act
+        String response = entitySpy.sendMessage(sender, "cool test message");
+
+        // assert
+        assertNull(response);
+    }
 
     @Test
      void testEntityAction() {
