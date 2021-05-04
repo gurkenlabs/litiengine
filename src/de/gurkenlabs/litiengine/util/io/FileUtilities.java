9f702ae0e42a36dc576bbe06473b8757da18ee46
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2021-05-04 15:58:11 -0400

    update humanReadableByteCount

--- src/de/gurkenlabs/litiengine/util/io/FileUtilities.java
+++ src/de/gurkenlabs/litiengine/util/io/FileUtilities.java
@@ -202,10 +202,15 @@ public final class FileUtilities {
   }
   
   public static String humanReadableByteCount(long bytes) {
-    int unit = 1024;
+    return humanReadableByteCount(bytes, false);
+  }
+  
+  public static String humanReadableByteCount(long bytes, boolean decimal) {
+    int unit = decimal ? 1000 : 1024;
     if (bytes < unit) return bytes + " bytes";
     int exp = (int) (Math.log(bytes) / Math.log(unit));
-    String pre = new String[] {"kibi", "mebi", "gibi", "tebi", "pebi", "exbi"}[exp-1];
-    return String.format("%.1f %sbytes", bytes / Math.pow(unit, exp), pre);
+    String pre = new String[] {"K", "M", "G", "T", "P", "E"}[exp-1];
+    pre = decimal ? pre : pre + "i";
+    return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
   }
 }
