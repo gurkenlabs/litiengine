571afccbad35e794054be398f4ff515151c31a48
Niels Kübler <niels.kuebler@uzh.ch>
2021-05-04 16:10:01 +0200

    Removed duplicated import from AlphanumComparatorTests

--- tests/de/gurkenlabs/litiengine/util/io/AlphanumComparatorTests.java
+++ tests/de/gurkenlabs/litiengine/util/io/AlphanumComparatorTests.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import de.gurkenlabs.litiengine.util.AlphanumComparator;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Comparator;
