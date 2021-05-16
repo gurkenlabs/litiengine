0f7a5e9ea1fc655ae3c14a59e843c6b7f6a03696
ddreimane <dreimane.dace@gmail.com>
2021-05-16 15:47:58 +0200

    Refactor FileUtilitiesTests code

--- tests/de/gurkenlabs/litiengine/util/io/FileUtilitiesTests.java
+++ tests/de/gurkenlabs/litiengine/util/io/FileUtilitiesTests.java
@@ -10,62 +10,65 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 public class FileUtilitiesTests {
 
-  @Test
-  public void testCombinePaths() {
-    String path1 = "/test/test2/";
-    String relativePath = "./test/test2";
-    String relativePath2 = "../test/test2/";
-    String relativePath3 = "../somepath/123/456/";
+  @ParameterizedTest(name = "testCombinePaths path1={0}, path2={1}, expectedValue={2}")
+  @MethodSource("getCombinedPaths")
+  public void testCombinePaths(String path1, String path2, String expectedValue) {
 
-    String otherPath = "somepath/123/456/";
+    String combined = FileUtilities.combine(path1, path2);
+    assertEquals(expectedValue, combined);
 
-    String somefile = "../file.txt";
-    String someOtherFile = "file.txt";
-
-    String combined = FileUtilities.combine(path1, otherPath);
-    String combined2 = FileUtilities.combine(otherPath, relativePath);
-    String combined3 = FileUtilities.combine(relativePath3, relativePath);
-    String combined4 = FileUtilities.combine(relativePath2, relativePath);
-    String combined5 = FileUtilities.combine(relativePath2, relativePath, relativePath3);
-    String combined6 = FileUtilities.combine(otherPath, somefile);
-    String combined7 = FileUtilities.combine(otherPath, someOtherFile);
-
-    assertEquals("/test/test2/somepath/123/456/", combined);
-    assertEquals("somepath/123/456/test/test2", combined2);
-    assertEquals("../somepath/123/456/test/test2", combined3);
-    assertEquals("../test/test2/test/test2", combined4);
-    assertEquals("../test/test2/somepath/123/456/", combined5);
-    assertEquals("somepath/123/file.txt", combined6);
-    assertEquals("somepath/123/456/file.txt", combined7);
   }
 
-  @Test
-  public void testCombinePathsWin() {
-    String winPath = "\\test\\test2\\";
-    String linuxPath = "somepath/123/456/";
+  private static Stream<Arguments> getCombinedPaths() {
+    return Stream.of(
+            Arguments.of("/test/test2/", "somepath/123/456/", "/test/test2/somepath/123/456/"),
+            Arguments.of("somepath/123/456/", "./test/test2", "somepath/123/456/test/test2"),
+            Arguments.of("../somepath/123/456/", "./test/test2", "../somepath/123/456/test/test2"),
+            Arguments.of("../test/test2/", "./test/test2", "../test/test2/test/test2"),
+            Arguments.of("somepath/123/456/", "../file.txt", "somepath/123/file.txt"),
+            Arguments.of("somepath/123/456/", "file.txt", "somepath/123/456/file.txt"));
+  }
 
-    String winPath2 = "test\\test2\\";
+  @ParameterizedTest(name = "testCombinePaths path1={0}, path2={1}, expectedValue={2}")
+  @MethodSource("getCombinedThreePaths")
+  public void testCombineThreePaths(String path1, String path2, String path3, String expectedValue) {
+    String combined = FileUtilities.combine(path1, path2, path3);
+    assertEquals(expectedValue, combined);
+  }
 
-    String linuxPath3 = "../somepath/123/456/";
-    String winPath3 = "..\\test\\test2\\";
-    String somefile = "..\\file.txt";
-    String someOtherFile = "file.txt";
+  private static Stream<Arguments> getCombinedThreePaths() {
+    return Stream.of(
+            Arguments.of("../test/test2/", "./test/test2", "../somepath/123/456/", "../test/test2/somepath/123/456/"));
+  }
 
-    String combined = FileUtilities.combine(winPath, linuxPath);
-    String combined2 = FileUtilities.combine(linuxPath, winPath2);
-    String combined3 = FileUtilities.combine(linuxPath3, winPath2);
-    String combined4 = FileUtilities.combine(winPath3, winPath2);
-    String combined5 = FileUtilities.combine(winPath3, winPath2, linuxPath);
-    String combined6 = FileUtilities.combine(linuxPath, somefile);
-    String combined7 = FileUtilities.combine(linuxPath, someOtherFile);
+  @ParameterizedTest(name = "testCombinePaths path1={0}, path2={1}, expectedValue={2}")
+  @MethodSource("getCombinePathsWin")
+  public void testCombineThreePathsWin(String path1, String path2, String expectedValue) {
+    String combined = FileUtilities.combine(path1, path2);
+    assertEquals(expectedValue, combined);
+  }
 
-    assertEquals("/test/test2/somepath/123/456/", combined);
-    assertEquals("somepath/123/456/test/test2/", combined2);
-    assertEquals("../somepath/123/456/test/test2/", combined3);
-    assertEquals("../test/test2/test/test2/", combined4);
-    assertEquals("../test/test2/test/test2/somepath/123/456/", combined5);
-    assertEquals("somepath/123/file.txt", combined6);
-    assertEquals("somepath/123/456/file.txt", combined7);
+  private static Stream<Arguments> getCombinePathsWin() {
+    return Stream.of(
+            Arguments.of("\\test\\test2\\", "somepath/123/456/", "/test/test2/somepath/123/456/"),
+            Arguments.of("somepath/123/456/", "test\\test2\\", "somepath/123/456/test/test2/"),
+            Arguments.of("../somepath/123/456/", "test\\test2\\", "../somepath/123/456/test/test2/"),
+            Arguments.of("..\\test\\test2\\", "test\\test2\\", "../test/test2/test/test2/"),
+            Arguments.of("somepath/123/456/", "..\\file.txt", "somepath/123/file.txt"),
+            Arguments.of("somepath/123/456/", "file.txt", "somepath/123/456/file.txt")
+    );
+  }
+
+  @ParameterizedTest(name = "testCombinePaths path1={0}, path2={1}, expectedValue={2}")
+  @MethodSource("getCombineThreePathsWin")
+  public void testCombineThreePathsWin(String path1, String path2, String path3, String expectedValue) {
+    String combined = FileUtilities.combine(path1, path2, path3);
+    assertEquals(expectedValue, combined);
+  }
+
+  private static Stream<Arguments> getCombineThreePathsWin() {
+    return Stream.of(
+            Arguments.of("..\\test\\test2\\", "test\\test2\\", "somepath/123/456/", "../test/test2/test/test2/somepath/123/456/"));
   }
 
   @Test
@@ -88,89 +91,76 @@ public class FileUtilitiesTests {
 
   private static Stream<Arguments> getFileNames() {
     return Stream.of(
-        Arguments.of("C:\\test\\test2\\test.txt", "test"),
-        Arguments.of("/somepath/123/456/test.txt", "test"),
-        Arguments.of("/somepath/123/456/test", "test"),
-        Arguments.of("/somepath/123/456/", ""),
-        Arguments.of("/somep.ath/1.23/45.6/test.txt", "test"),
-        Arguments.of("/somep.ath/1.23/45.6/", ""));
+            Arguments.of("C:\\test\\test2\\test.txt", "test"),
+            Arguments.of("/somepath/123/456/test.txt", "test"),
+            Arguments.of("/somepath/123/456/test", "test"),
+            Arguments.of("/somepath/123/456/", ""),
+            Arguments.of("/somep.ath/1.23/45.6/test.txt", "test"),
+            Arguments.of("/somep.ath/1.23/45.6/", ""));
   }
 
-  @Test
-  public void testGetExtension() {
-    String file1 = "C:\\test\\test2\\test.txt";
-    String file2 = "/somepath/123/456/test.123456789";
-    String file3 = "/somepath/123/456/test";
-    String file4 = "/somepath/123/456/";
-    String file5 = "/somepath/1.23/4.56/";
-
-    String extension = FileUtilities.getExtension(file1);
-    String extension2 = FileUtilities.getExtension(file2);
-    String extension3 = FileUtilities.getExtension(file3);
-    String extension4 = FileUtilities.getExtension(file4);
-    String extension5 = FileUtilities.getExtension(file5);
-
-    assertEquals("txt", extension);
-    assertEquals("123456789", extension2);
-    assertEquals("", extension3);
-    assertEquals("", extension4);
-    assertEquals("", extension5);
+  @ParameterizedTest(name = "testGetFileName file={0}, expectedValue={1}")
+  @MethodSource("getGetExtension")
+  public void testGetExtension(String file, String expectedValue) {
+    String extension = FileUtilities.getExtension(file);
+    assertEquals(expectedValue, extension);
   }
 
-  @Test
-  public void testHumanReadableByteCount() {
-    long zero = 0;
-    long max = Long.MAX_VALUE;
-    {
-      long lessThanKibibyte = 1023;
-      long kibibyte = 1024;
-      long moreThanKibibyte = 1025;
-      long mebibyte = 1048576;
-      long gibibyte = 1073741824;
-      long tebibyte = 1099511627776l;
-      long pebibyte = 1125899906842624l;
-      long exbibyte = 1152921504606846976l;
+  private static Stream<Arguments> getGetExtension() {
+    return Stream.of(
+            Arguments.of("C:\\test\\test2\\test.txt", "txt"),
+            Arguments.of("/somepath/123/456/test.123456789", "123456789"),
+            Arguments.of("/somepath/123/456/test", ""),
+            Arguments.of("/somepath/123/456/", ""),
+            Arguments.of("/somepath/1.23/4.56/", ""));
+  }
 
-      assertEquals("0 bytes", FileUtilities.humanReadableByteCount(zero));
-      assertEquals("1023 bytes", FileUtilities.humanReadableByteCount(lessThanKibibyte));
-      assertEquals("1.0 KiB", FileUtilities.humanReadableByteCount(kibibyte));
-      assertEquals("1.0 KiB", FileUtilities.humanReadableByteCount(moreThanKibibyte));
-      assertEquals("1.5 KiB", FileUtilities.humanReadableByteCount(kibibyte + kibibyte / 2));
-      assertEquals("1.0 MiB", FileUtilities.humanReadableByteCount(mebibyte));
-      assertEquals("1.0 GiB", FileUtilities.humanReadableByteCount(gibibyte));
-      assertEquals("1.0 TiB", FileUtilities.humanReadableByteCount(tebibyte));
-      assertEquals("1.0 PiB", FileUtilities.humanReadableByteCount(pebibyte));
-      assertEquals("1.5 PiB", FileUtilities.humanReadableByteCount(pebibyte + pebibyte / 2));
-      assertEquals("1.0 EiB", FileUtilities.humanReadableByteCount(exbibyte));
-      assertEquals("8.0 EiB", FileUtilities.humanReadableByteCount(max));
-
-      assertEquals("1.2 EB", FileUtilities.humanReadableByteCount(exbibyte, true));
+  @ParameterizedTest(name = "testHumanReadableByteCount bytes={0}, expectedValue={1}")
+  @MethodSource("getGetHumanReadableByteCount")
+  public void testHumanReadableByteCount(long bytes, String expectedValue) {
+      assertEquals(expectedValue, FileUtilities.humanReadableByteCount(bytes));
     }
 
-    {
-      long lessThanKilobyte = 999;
-      long kilobyte = 1000;
-      long moreThanOneKilobyte = 1001;
-      long megabyte = 1000000;
-      long gigabyte = 1000000000;
-      long terabyte = 1000000000000l;
-      long petabyte = 1000000000000000l;
-      long exabyte = 1000000000000000000l;
 
-      assertEquals("0 bytes", FileUtilities.humanReadableByteCount(zero, true));
-      assertEquals("999 bytes", FileUtilities.humanReadableByteCount(lessThanKilobyte, true));
-      assertEquals("1.0 KB", FileUtilities.humanReadableByteCount(moreThanOneKilobyte, true));
-      assertEquals("1.0 KB", FileUtilities.humanReadableByteCount(kilobyte, true));
-      assertEquals("1.5 KB", FileUtilities.humanReadableByteCount(kilobyte + kilobyte / 2, true));
-      assertEquals("1.0 MB", FileUtilities.humanReadableByteCount(megabyte, true));
-      assertEquals("1.0 GB", FileUtilities.humanReadableByteCount(gigabyte, true));
-      assertEquals("1.0 TB", FileUtilities.humanReadableByteCount(terabyte, true));
-      assertEquals("1.0 PB", FileUtilities.humanReadableByteCount(petabyte, true));
-      assertEquals("1.5 PB", FileUtilities.humanReadableByteCount(petabyte + petabyte / 2, true));
-      assertEquals("1.0 EB", FileUtilities.humanReadableByteCount(exabyte, true));
-      assertEquals("9.2 EB", FileUtilities.humanReadableByteCount(max, true));
+  private static Stream<Arguments> getGetHumanReadableByteCount() {
+    return Stream.of(
+            Arguments.of(0, "0 bytes"),
+            Arguments.of(1023, "1023 bytes"),
+            Arguments.of(1024, "1.0 KiB"),
+            Arguments.of(1025, "1.0 KiB"),
+            Arguments.of(1024 + 1024 / 2, "1.5 KiB"),
+            Arguments.of(1048576, "1.0 MiB"),
+            Arguments.of(1073741824, "1.0 GiB"),
+            Arguments.of(1099511627776l, "1.0 TiB"),
+            Arguments.of(1125899906842624l, "1.0 PiB"),
+            Arguments.of(1125899906842624l + 1125899906842624l / 2, "1.5 PiB"),
+            Arguments.of(1152921504606846976l, "1.0 EiB"),
+            Arguments.of(Long.MAX_VALUE, "8.0 EiB")
+    );
+  }
 
-      assertEquals("888.2 PiB", FileUtilities.humanReadableByteCount(exabyte, false));
-    }
+  @ParameterizedTest(name = "testHumanReadableByteCountDecimal bytes={0}, isDecimal={1}, expectedValue={2}")
+  @MethodSource("getGetHumanReadableByteCountDecimal")
+  public void testHumanReadableByteCountDecimal(long bytes, boolean isDecimal, String expectedValue) {
+    assertEquals(expectedValue, FileUtilities.humanReadableByteCount(bytes, isDecimal));
+  }
+
+  private static Stream<Arguments> getGetHumanReadableByteCountDecimal() {
+    return Stream.of(
+            Arguments.of(1152921504606846976l, true, "1.2 EB"),
+            Arguments.of(0, true, "0 bytes"),
+            Arguments.of(999, true, "999 bytes"),
+            Arguments.of(1001, true, "1.0 KB"),
+            Arguments.of(1000, true, "1.0 KB"),
+            Arguments.of(1000+1000/2, true, "1.5 KB"),
+            Arguments.of(1000000, true, "1.0 MB"),
+            Arguments.of(1000000000, true, "1.0 GB"),
+            Arguments.of(1000000000000l, true, "1.0 TB"),
+            Arguments.of(1000000000000000l, true, "1.0 PB"),
+            Arguments.of(1000000000000000l+1000000000000000l/2, true, "1.5 PB"),
+            Arguments.of(1000000000000000000l, true, "1.0 EB"),
+            Arguments.of(Long.MAX_VALUE, true, "9.2 EB"),
+            Arguments.of(1000000000000000000l, false, "888.2 PiB")
+    );
   }
 }
