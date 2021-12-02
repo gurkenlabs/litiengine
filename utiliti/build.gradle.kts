885824c5be4450dab69e64df7af8f99d5c97c765
steffen-wilke <mail@steffen-wilke.com>
2021-12-02 22:36:33 +0100

    Fix indentation and broken resource directory.

--- utiliti/build.gradle.kts
+++ utiliti/build.gradle.kts
@@ -1,8 +1,8 @@
 import java.util.Calendar
 
 plugins {
-    `java-library`
-    id("org.beryx.runtime")
+  `java-library`
+  id("org.beryx.runtime")
 }
 
 description = """
@@ -10,57 +10,57 @@ description = """
 """.trimIndent()
 
 sourceSets {
-    main {
-        resources {
-            srcDir("localization")
-        }
+  main {
+    resources {
+      srcDir("src/main/localization")
     }
+  }
 }
 
 application {
-    mainClass.set("de.gurkenlabs.utiliti.Program")
-    applicationDistribution.into(executableDir) {
-        from("src/dist")
-    }
+  mainClass.set("de.gurkenlabs.utiliti.Program")
+  applicationDistribution.into(executableDir) {
+    from("src/dist")
+  }
 }
 
 dependencies {
-    implementation(projects.litiengineCore)
-    implementation(libs.darklaf.core)
-    testImplementation(projects.litiengineShared)
+  implementation(projects.litiengineCore)
+  implementation(libs.darklaf.core)
+  testImplementation(projects.litiengineShared)
 }
 
 tasks {
-    test {
-        useJUnitPlatform()
-    }
+  test {
+    useJUnitPlatform()
+  }
 }
 
 runtime {
-    options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
-    modules.set(listOf("java.desktop", "java.logging", "java.datatransfer", "java.management", "java.xml"))
+  options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
+  modules.set(listOf("java.desktop", "java.logging", "java.datatransfer", "java.management", "java.xml"))
 
-    launcher {
-        noConsole = false
-    }
+  launcher {
+    noConsole = false
+  }
 
-    jpackage {
-        appVersion = project.version.toString().run {
-            val invalidIndex = indexOfFirst { !it.isDigit() && it != '.' }
-            if (invalidIndex >= 0) {
-                substring(0, invalidIndex)
-            } else this
-        }
-        skipInstaller = true
-        imageOptions.addAll(
-            listOf(
-                "--icon", project.file("src/dist/pixel-icon-utiliti.ico").path,
-                "--description", project.description,
-                "--copyright", "2020-${Calendar.getInstance().get(Calendar.YEAR)} gurkenlabs.de",
-                "--vendor", "gurkenlabs.de",
-                "--java-options", "-Xms256m",
-                "--java-options", "-Xmx1024m",
-            )
-        )
+  jpackage {
+    appVersion = project.version.toString().run {
+      val invalidIndex = indexOfFirst { !it.isDigit() && it != '.' }
+      if (invalidIndex >= 0) {
+        substring(0, invalidIndex)
+      } else this
     }
+    skipInstaller = true
+    imageOptions.addAll(
+      listOf(
+        "--icon", project.file("src/dist/pixel-icon-utiliti.ico").path,
+        "--description", project.description,
+        "--copyright", "2020-${Calendar.getInstance().get(Calendar.YEAR)} gurkenlabs.de",
+        "--vendor", "gurkenlabs.de",
+        "--java-options", "-Xms256m",
+        "--java-options", "-Xmx1024m",
+      )
+    )
+  }
 }
