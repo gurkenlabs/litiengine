bd1f6878ca9bceb6052ae69e0b8bf871420a76d1
Steffen <mail@steffen-wilke.com>
2023-12-27 15:41:33 +0100

    Support TMX changes on layers
    - Support repeat properties for image layers.
    - Support double values for layer offset
    - Support parallax factors for layers
    - Support tintcolor property for layers
    - Add test for image layers
    
    Fixes #462

--- litiengine/src/main/java/de/gurkenlabs/litiengine/environment/tilemap/IImageLayer.java
+++ litiengine/src/main/java/de/gurkenlabs/litiengine/environment/tilemap/IImageLayer.java
@@ -9,27 +9,12 @@ public interface IImageLayer extends ICustomPropertyProvider, ILayer {
    *
    * @return the image
    */
-  IMapImage getImage();
+  public IMapImage getImage();
 
   /**
    * Gets the transparent color.
    *
    * @return the transparent color
    */
-  Color getTransparentColor();
-
-  /**
-   * Gets a value indicating whether the image should be rendered repeatedly horizontally.
-   *
-   * @return True if the image should be repeated horizontally; Otherwise false.
-   */
-  boolean repeatHorizontally();
-
-
-  /**
-   * Gets a value indicating whether the image should be rendered repeatedly vertically.
-   *
-   * @return True if the image should be repeated vertically; Otherwise false.
-   */
-  boolean repeatVertically();
+  public Color getTransparentColor();
 }
