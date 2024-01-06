bafa8d741a572a6982eb2c3db66166637f702546
Steffen Wilke <mail@steffen-wilke.com>
2024-01-06 11:03:12 +0100

    Add some more tests and simplify FileReader

--- litiengine/src/test/java/de/gurkenlabs/litiengine/sound/Mp3FileReaderTests.java
+++ litiengine/src/test/java/de/gurkenlabs/litiengine/sound/Mp3FileReaderTests.java
@@ -10,8 +10,15 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
 
 import static junit.framework.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class Mp3FileReaderTests {
+
+  @Test
+  void invalidFilesAreRecognized() {
+    var fileReader = new Mp3FileReader();
+    assertThrows(UnsupportedAudioFileException.class, () -> fileReader.getAudioFileFormat(Resources.getLocation("de/gurkenlabs/litiengine/resources/bop.wav").openStream()));
+  }
 
   @Test
   void testReadFile() throws IOException, UnsupportedAudioFileException, InterruptedException {
--- litiengine/src/test/java/de/gurkenlabs/litiengine/sound/spi/mp3/MpegFrameTests.java
+++ litiengine/src/test/java/de/gurkenlabs/litiengine/sound/spi/mp3/MpegFrameTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.nio.ByteBuffer;
 
-import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.*;
 
 class MpegFrameTests {
   @Test
@@ -13,8 +13,17 @@ class MpegFrameTests {
     var bytes = ByteBuffer.allocate(4).putInt((int)Long.parseLong("11111111111110110011100011000100", 2)).array();
     var frame = new MpegFrame(bytes[0], bytes[1], bytes[2], bytes[3]);
 
+    assertEquals("1.0", frame.getVersion());
     assertEquals(Mpeg.LAYERS[3], frame.getLayer());
     assertEquals(32000, frame.getSampleRate());
     assertEquals(48, frame.getBitRate());
+    assertEquals(Mpeg.CHANNEL_MODE_MONO, frame.getChannelMode());
+    assertEquals(Mpeg.MODE_EXTENSION_NA, frame.getModeExtension());
+    assertEquals(Mpeg.EMPHASIS_NONE, frame.getEmphasis());
+    assertFalse(frame.hasPadding());
+    assertTrue(frame.isProtection());
+    assertFalse(frame.isPrivate());
+    assertFalse(frame.isCopyright());
+    assertTrue(frame.isOriginal());
   }
 }
