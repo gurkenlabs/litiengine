48d6a7eeec4f9447404d8bdbbca57208b00c7a68
Steffen Wilke <mail@steffen-wilke.com>
2026-07-05 11:31:52 +0200

    Handle missing ambient light preview image

--- utiliti/src/main/java/de/gurkenlabs/utiliti/view/components/AmbientLightPreviewPanel.java
+++ utiliti/src/main/java/de/gurkenlabs/utiliti/view/components/AmbientLightPreviewPanel.java
@@ -6,12 +6,15 @@ import de.gurkenlabs.litiengine.resources.Resources;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 public class AmbientLightPreviewPanel extends JPanel {
   private Color ambientColor = AmbientLight.DEFAULT_COLOR;
   private Color staticShadowColor = StaticShadow.DEFAULT_COLOR;
+  private Image previewImage;
+  private boolean previewImageLoadFailed;
 
   public AmbientLightPreviewPanel() {
 
@@ -56,11 +59,38 @@ public class AmbientLightPreviewPanel extends JPanel {
   @Override
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
-    g.drawImage(Resources.images().get("ambientlight-preview.png"), 0, 0, 300, 116, null);
+    Image preview = getPreviewImage();
+    if (preview != null) {
+      g.drawImage(preview, 0, 0, 300, 116, null);
+    } else {
+      paintFallbackPreview(g);
+    }
     g.setColor(this.staticShadowColor);
     g.fillRect(0, 38, 300, 40);
     g.setColor(this.ambientColor);
     g.fillRect(0, 0, 82, 116);
     g.fillRect(156, 0, 72, 116);
+  }
+
+  private Image getPreviewImage() {
+    if (this.previewImage != null || this.previewImageLoadFailed) {
+      return this.previewImage;
+    }
+
+    try {
+      this.previewImage = Resources.images().get("ambientlight-preview.png");
+    } catch (RuntimeException e) {
+      this.previewImageLoadFailed = true;
+    }
+    return this.previewImage;
+  }
+
+  private static void paintFallbackPreview(Graphics g) {
+    g.setColor(new Color(42, 44, 48));
+    g.fillRect(0, 0, 300, 116);
+    g.setColor(new Color(62, 64, 70));
+    g.fillRect(0, 76, 300, 40);
+    g.setColor(new Color(82, 84, 90));
+    g.fillRect(112, 20, 70, 56);
   }
 }
